30200427ab9d924f67a7f98b7cbef6dc64b581c5
Tero Marttila <tero.marttila@kontena.io>
2018-03-06 16:33:40 +0200

    get fancy with optimizing max-surge for larger clusters

--- lib/kupo/phases/configure_dns.rb
+++ lib/kupo/phases/configure_dns.rb
@@ -9,12 +9,49 @@ module Kupo::Phases
     end
 
     def call
-      logger.info(@master.address) { "Patching kube-dns addon..." }
-      patch_kubedns(replicas: @config.dns_replicas)
+
+      patch_kubedns(
+        replicas: @config.dns_replicas,
+        max_surge: self.max_surge,
+        max_unavailable: self.max_unavailable,
+      )
+    end
+
+    # @return [Integer]
+    def max_surge
+      replicas = @config.dns_replicas
+      nodes = @config.hosts.length
+
+      if replicas == nodes
+        # cannot create any extra replicas
+        0
+      elsif nodes <= replicas * 1.5
+        # at most one per extra node
+        nodes - replicas
+      else
+        # half
+        (replicas * 0.5).floor
+      end
+    end
+
+    # @return [Integer]
+    def max_unavailable
+      replicas = @config.dns_replicas
+
+      if replicas == 1
+        # must allow taking down all replicas
+        1
+      else
+        # half
+        (replicas * 0.5).floor
+      end
     end
 
     # @param replicas [Integer]
-    def patch_kubedns(replicas: )
+    # @param nodes [Integer]
+    def patch_kubedns(replicas: , max_surge: , max_unavailable: )
+      logger.info(@master.address) { "Patching kube-dns addon with #{replicas} replicas (max-surge #{max_surge}, max-unavailable #{max_unavailable})..." }
+
       kube_client = Kupo::Kube.update_resource(@master.address, Kubeclient::Resource.new({
         apiVersion: 'extensions/v1beta1',
         kind: 'Deployment',
@@ -27,8 +64,8 @@ module Kupo::Phases
           strategy: {
             type: "RollingUpdate",
             rollingUpdate: {
-              maxSurge: 0, # cannot have more than two pods for a two-node cluster
-              maxUnavailable: [1, (replicas * 0.5).floor].max, # at least one for a single-node cluster
+              maxSurge: max_surge, # must be zero for a two-node cluster
+              maxUnavailable: max_unavailable, # must be at least one for a single-node cluster
             },
           },
           template: {
