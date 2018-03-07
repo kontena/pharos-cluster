0adf7ae5de68a8151c915893185bbc7152988d8b
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-03-07 11:53:42 +0200

    Build MacOS package in Travis (#32)

---
+++ build/travis/macos.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -ue
+
+brew install squashfs
+curl -L http://enclose.io/rubyc/rubyc-darwin-x64.gz | gunzip > /usr/local/bin/rubyc
+chmod +x /usr/local/bin/rubyc
+rubyc -o kupo kupo
+./kupo version --all
+
+# ship to github
+curl -sL https://github.com/aktau/github-release/releases/download/v0.7.2/darwin-amd64-github-release.tar.bz2 | tar -xjO > /usr/local/bin/github-release
+chmod +x /usr/local/bin/github-release
+/usr/local/bin/github-release upload \
+    --user kontena \
+    --repo kupo \
+    --tag $TRAVIS_TAG \
+    --name "kupo-darwin-amd64" \
+    --file ./kupo
