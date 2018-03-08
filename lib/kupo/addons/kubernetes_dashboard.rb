9b4c00ae9c3aeef5a4db9e84288892d192461178
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-03-08 19:38:32 +0200

    kube dashboard addon

---
+++ lib/kupo/addons/kubernetes_dashboard.rb
@@ -0,0 +1,11 @@
+module Kupo
+  module Addons
+    class KubeDashboard < Kupo::Addon
+
+      name 'kubernetes-dashboard'
+      version '1.8.3'
+      license 'Apache License 2.0'
+
+    end
+  end
+end
