bd9780408df3b3f531f87547215e4e350d6337b6
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-03-08 15:45:54 +0200

    Make Kupo::Erb take a file path instead of file content

--- lib/kupo/erb.rb
+++ lib/kupo/erb.rb
@@ -5,12 +5,24 @@ require 'ostruct'
 
 module Kupo
   class Erb
-    def initialize(template)
-      @template = template
+    def initialize(path = nil)
+      @path = path
+    end
+
+    def erb?
+      File.fnmatch('*.erb', @path)
+    end
+
+    def template
+      File.read(path)
     end
 
     def render(vars = {})
-      ERB.new(@template, nil, '%<>-').result(OpenStruct.new(vars).instance_eval { binding })
+      if erb?
+        template
+      else
+        ERB.new(template, nil, '%<>-').result(OpenStruct.new(vars).instance_eval { binding })
+      end
     end
   end
 end
