8c3a68d0cab99da6e64f21f7d2742cc5067f1bb0
Jussi Nummelin <jussi@kontena.io>
2018-03-15 12:28:06 +0200

    add note why we need the kubeadm beta binary

--- lib/kupo/scripts/configure-kube.sh
+++ lib/kupo/scripts/configure-kube.sh
@@ -12,5 +12,8 @@ apt-get install -y kubelet=<%= kube_version %>-00 kubeadm=<%= kube_version %>-00
 apt-mark hold kubelet kubelet kubeadm kubectl
 
 # Hack to get 1.10.beta.3 kubeadm in place
+# Needed to be able to configure cri socket in the config file
+# See: https://github.com/kubernetes/kubernetes/pull/59057
+# FIXME Remove when we're using official 1.10 kubeadm
 curl -o /usr/bin/kubeadm https://storage.googleapis.com/kubernetes-release/release/v1.10.0-beta.3/bin/linux/amd64/kubeadm
 chmod +x /usr/bin/kubeadm
