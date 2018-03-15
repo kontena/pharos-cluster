d74f95ba2272b7fdee8eb4b8820f5b1455dc5d92
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-03-15 14:16:51 +0200

    remove ntp

--- lib/kupo/scripts/configure-essentials.sh
+++ lib/kupo/scripts/configure-essentials.sh
@@ -6,5 +6,5 @@ dpkg -l ntp apt-transport-https software-properties-common > /dev/null
 
 if [  $? != 0 ]; then
     apt-get update -y
-    apt-get install -y ntp apt-transport-https software-properties-common
+    apt-get install -y apt-transport-https software-properties-common
 fi
