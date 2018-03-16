d3db936f4c0c47395385eb8ac5c5d50ff5374a99
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-03-16 14:45:36 +0200

    Tweak run script

--- build/drone/run_tests.sh
+++ build/drone/run_tests.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-cd ..
+#!/bin/sh
 
 bundle install --path bundler
 bundle exec rspec spec/
@@ -8,6 +6,6 @@ SPEC_RETURN=$?
 
 PRONTO_GITHUB_ACCESS_TOKEN=$GITHUB_TOKEN \
   PRONTO_PULL_REQUEST_ID=$DRONE_PULL_REQUEST \
-  pronto run -f github_status github_pr -c origin/master
+  bundle exec pronto run -f github_status github_pr -c origin/master
 
 exit $SPEC_RETURN
