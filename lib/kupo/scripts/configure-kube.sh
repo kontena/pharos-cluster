019b396c9e5ff9ef0527a6b6a339f0eefd8ecaff
Jussi Nummelin <jussi@kontena.io>
2018-03-16 16:32:49 +0200

    add kubelet dropin conf

--- lib/kupo/scripts/configure-kube.sh
+++ lib/kupo/scripts/configure-kube.sh
@@ -7,6 +7,23 @@ if [ "$(kubelet --version)" = "Kubernetes v<%= kube_version %>" ]; then
     exit 0
 fi
 
+# Put in the basic kubelet config, later kubeadm commands will make things work properly
+mkdir -p /etc/systemd/system/kubelet.service.d/
+cat <<EOF >/etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+[Service]
+Environment="KUBELET_KUBECONFIG_ARGS=--bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf --kubeconfig=/etc/kubernetes/kubelet.conf"
+Environment="KUBELET_SYSTEM_PODS_ARGS=--pod-manifest-path=/etc/kubernetes/manifests --allow-privileged=true"
+Environment="KUBELET_NETWORK_ARGS=--network-plugin=cni --cni-conf-dir=/etc/cni/net.d --cni-bin-dir=/opt/cni/bin"
+Environment="KUBELET_DNS_ARGS=--cluster-dns=10.96.0.10 --cluster-domain=cluster.local"
+Environment="KUBELET_AUTHZ_ARGS=--authorization-mode=Webhook --client-ca-file=/etc/kubernetes/pki/ca.crt"
+Environment="KUBELET_CADVISOR_ARGS=--cadvisor-port=0"
+Environment="KUBELET_CERTIFICATE_ARGS=--rotate-certificates=true --cert-dir=/var/lib/kubelet/pki"
+ExecStart=
+ExecStart=/usr/bin/kubelet $KUBELET_KUBECONFIG_ARGS $KUBELET_SYSTEM_PODS_ARGS $KUBELET_NETWORK_ARGS $KUBELET_DNS_ARGS $KUBELET_AUTHZ_ARGS $KUBELET_CADVISOR_ARGS $KUBELET_CERTIFICATE_ARGS $KUBELET_EXTRA_ARGS
+EOF
+
+sleep 60
+
 apt-mark unhold kubelet kubectl
 apt-get install -y kubelet=<%= kube_version %>-00 kubectl=<%= kube_version %>-00
 apt-mark hold kubelet kubelet kubectl
@@ -14,3 +31,4 @@ apt-mark hold kubelet kubelet kubectl
 # Get kubeadm binary directly
 curl -o /usr/bin/kubeadm https://storage.googleapis.com/kubernetes-release/release/v<%= kubeadm_version %>/bin/linux/<%= arch %>/kubeadm
 chmod +x /usr/bin/kubeadm
+
