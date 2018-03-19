4f4931435bbe582e4b36bc8b669f87e540052af2
Tero Marttila <tero.marttila@kontena.io>
2018-03-19 14:57:19 +0200

    strip out unnecessary error handling

--- lib/kupo/phases/configure_client.rb
+++ lib/kupo/phases/configure_client.rb
@@ -14,13 +14,9 @@ module Kupo::Phases
       config_file = File.join(config_dir, @master.address)
       logger.info { "Fetching kubectl config ..." }
       config_data = ssh.file_contents("/etc/kubernetes/admin.conf")
-      if config_data.nil?
-        logger.error { "Failed to fetch configuration file via SSH" }
-        raise "Failed to configure client"
-      else
-        File.write(config_file, config_data.gsub(/(server: https:\/\/)(.+)(:6443)/, "\\1#{@master.address}\\3"))
-        logger.info { "Configuration saved to #{config_file}"}
-      end
+      
+      File.write(config_file, config_data.gsub(/(server: https:\/\/)(.+)(:6443)/, "\\1#{@master.address}\\3"))
+      logger.info { "Configuration saved to #{config_file}"}
     end
 
     def config_dir
