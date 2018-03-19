8074477f36469601486003fc77877acfec2a31b6
Tero Marttila <tero.marttila@kontena.io>
2018-03-19 16:10:31 +0200

    mergefix

--- lib/kupo/phases/configure_client.rb
+++ lib/kupo/phases/configure_client.rb
@@ -13,7 +13,7 @@ module Kupo::Phases
       Dir.mkdir(config_dir, 0700) unless Dir.exists?(config_dir)
       config_file = File.join(config_dir, @master.address)
       logger.info { "Fetching kubectl config ..." }
-      config_data = ssh.read_file("/etc/kubernetes/admin.conf")
+      config_data = @ssh.read_file("/etc/kubernetes/admin.conf")
 
       File.write(config_file, config_data.gsub(/(server: https:\/\/)(.+)(:6443)/, "\\1#{@master.address}\\3"))
       logger.info { "Configuration saved to #{config_file}"}
