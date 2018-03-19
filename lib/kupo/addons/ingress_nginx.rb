b393af63f0b50ea6a35cf21eec9eb8de344504cd
Jussi Nummelin <jussi@kontena.io>
2018-03-19 16:07:25 +0200

    # This is a combination of 2 commits. # The first commit's message is:
    custom default backend
    
    add custom defaul backend plus config for the image
    
    handle image logic in erb
    
    # This is the 2nd commit message:
    
    fix arch to use name

--- lib/kupo/addons/ingress_nginx.rb
+++ lib/kupo/addons/ingress_nginx.rb
@@ -10,17 +10,20 @@ module Kupo
       struct {
         attribute :configmap, Kupo::Types::Hash
         attribute :node_selector, Kupo::Types::Hash
+        attribute :image, Kupo::Types::String
       }
 
       schema {
         optional(:configmap).filled(:hash?)
         optional(:node_selector).filled(:hash?)
+        optional(:image).filled(:str?)
       }
 
       def install
         apply_stack(
           configmap: config.configmap || {},
-          node_selector: config.node_selector
+          node_selector: config.node_selector,
+          image: config.image
         )
       end
     end
