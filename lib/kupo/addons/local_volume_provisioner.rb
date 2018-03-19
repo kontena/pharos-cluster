bc5f169a8fcf9ac8aaca9e7225c401e760ebe352
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-03-19 10:49:20 +0200

    add local-volume-provisioner addon

---
+++ lib/kupo/addons/local_volume_provisioner.rb
@@ -0,0 +1,11 @@
+module Kupo
+  module Addons
+    class LocalVolumeProvisioner < Kupo::Addon
+
+      name 'local-volume-provisioner'
+      version '2.0.0'
+      license 'Apache License 2.0'
+
+    end
+  end
+end
