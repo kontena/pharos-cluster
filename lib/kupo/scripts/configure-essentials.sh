0f905d21de2486c0760ded5779d2cc04b6d74711
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-03-20 20:25:49 +0200

    install required packages in configure-kube script

--- lib/kupo/scripts/configure-essentials.sh
+++ lib/kupo/scripts/configure-essentials.sh
@@ -4,5 +4,5 @@ set -e
 
 if ! dpkg -l apt-transport-https software-properties-common > /dev/null; then
     apt-get update -y
-    apt-get install -y apt-transport-https software-properties-common iproute2 socat util-linux mount ebtables ethtool
+    apt-get install -y apt-transport-https software-properties-common
 fi
