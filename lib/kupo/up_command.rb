c0bbb50062c2dad6740e1cc50776c2dca635b294
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-03-20 10:42:28 +0200

    Style/NumericPredicate: Use secs.positive? instead of secs > 0

--- lib/kupo/up_command.rb
+++ lib/kupo/up_command.rb
@@ -54,7 +54,7 @@ module Kupo
 
     def humanize_duration(secs)
       [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
-        if secs > 0
+        if secs.positive?
           secs, n = secs.divmod(count)
           "#{n.to_i} #{name}"
         end
