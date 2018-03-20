94dd4c5ade35389647c3edab48a60dca4642e098
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-03-20 12:19:46 +0200

    Release v0.3.0-rc.1 (#114)

--- build/drone/ubuntu_xenial.sh
+++ build/drone/ubuntu_xenial.sh
@@ -4,11 +4,10 @@ set -ue
 
 # build binary
 apt-get update -y
-apt-get install -y -q squashfs-tools build-essential ruby bison ruby-dev git-core curl
+apt-get install -y -q squashfs-tools build-essential ruby bison ruby-dev git-core texinfo curl
 curl -sL https://dl.bintray.com/kontena/ruby-packer/0.5.0-dev/rubyc-linux-amd64.gz | gunzip > /usr/local/bin/rubyc
 chmod +x /usr/local/bin/rubyc
 gem install bundler
-bundle install --path bundler
 rubyc -o kupo kupo
 ./kupo version --all
 
