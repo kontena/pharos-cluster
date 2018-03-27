2647de057e10e789fded37290244eb4a67dbce42
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-03-27 13:50:49 +0300

    Fashion crime

--- lib/kupo/configuration/ssh_options.rb
+++ lib/kupo/configuration/ssh_options.rb
@@ -1,19 +1,23 @@
-module Kupo::Configuration
-  class SshOptions < Dry::Struct
-    constructor_type :schema
+# frozen_string_literal: true
 
-    attribute :bind_address, Kupo::Types::Strict::String.optional
-    attribute :compression, Kupo::Types::Strict::Bool.default(true)
-    attribute :keepalive, Kupo::Types::Strict::Bool.default(false)
-    attribute :keepalive_interval, Kupo::Types::Strict::Int.default(300)
-    attribute :verbose, Kupo::Types::Strict::String.default('fatal')
-    attribute :port, Kupo::Types::Strict::Int.default(22)
-    attribute :host_key_alias, Kupo::Types::Strict::String.optional
-    attribute :global_known_hosts_file, Kupo::Types::Strict::String.optional
-    attribute :user_known_hosts_file, Kupo::Types::Strict::String.optional
+module Kupo
+  module Configuration
+    class SshOptions < Dry::Struct
+      constructor_type :schema
 
-    def to_h
-      super.merge(verbose: self.verbose.to_sym)
+      attribute :bind_address, Kupo::Types::Strict::String.optional
+      attribute :compression, Kupo::Types::Strict::Bool.default(true)
+      attribute :keepalive, Kupo::Types::Strict::Bool.default(false)
+      attribute :keepalive_interval, Kupo::Types::Strict::Int.default(300)
+      attribute :verbose, Kupo::Types::Strict::String.default('fatal')
+      attribute :port, Kupo::Types::Strict::Int.default(22)
+      attribute :host_key_alias, Kupo::Types::Strict::String.optional
+      attribute :global_known_hosts_file, Kupo::Types::Strict::String.optional
+      attribute :user_known_hosts_file, Kupo::Types::Strict::String.optional
+
+      def to_h
+        super.merge(verbose: verbose.to_sym)
+      end
     end
   end
 end
