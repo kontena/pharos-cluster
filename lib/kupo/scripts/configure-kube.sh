0863be2e41d57c7cdc97ca9c0d234f205bf1ed54
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-03-28 10:08:33 +0300

    don't exit if kubelet version matches

--- lib/kupo/scripts/configure-kube.sh
+++ lib/kupo/scripts/configure-kube.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-set -e
-
-if [ "$(kubelet --version)" = "Kubernetes v$KUBE_VERSION" ]; then
-    exit 0
-fi
+set -eu
 
 apt-mark unhold kubelet kubectl kubeadm
 apt-get install -y kubelet=${KUBE_VERSION}-00 kubectl=${KUBE_VERSION}-00 kubeadm=${KUBE_VERSION}-00
