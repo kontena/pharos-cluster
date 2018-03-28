d632f9d9a65a00b17a07689df12c2029cd515607
Jussi Nummelin <jnummelin@users.noreply.github.com>
2018-03-28 16:58:14 +0300

    Custom default backend (#109)

--- README.md
+++ README.md
@@ -86,11 +86,14 @@ ingress-nginx:
     disk: ssd
   configmap:
     load-balance: least_conn
+  default_backend:
+    image: my-custom-image:latest
 ```
 #### Options
 
 - `node_selector` - deployment node selector (map), deploys ingress only to matching nodes.
 - `configmap` - custom configuration (map). For all supported `configmap` options, see: https://github.com/kubernetes/ingress-nginx/blob/master/docs/user-guide/configmap.md
+- `default_backend.image` - custom image to be used as the default backend for the Nginx Ingress. Expected to fulfill the default backend [requirements](https://github.com/kubernetes/ingress-nginx#requirements). Leave empty to use Pharos' own default backend.
 
 ### Cert Manager
 
--- lib/kupo/addons/ingress_nginx.rb
+++ lib/kupo/addons/ingress_nginx.rb
@@ -10,17 +10,35 @@ module Kupo
       struct {
         attribute :configmap, Kupo::Types::Hash
         attribute :node_selector, Kupo::Types::Hash
+        attribute :default_backend, Kupo::Types::Hash
       }
 
       schema {
         optional(:configmap).filled(:hash?)
         optional(:node_selector).filled(:hash?)
+        optional(:default_backend).schema do
+          optional(:image).filled(:str?)
+        end
       }
+
+      DEFAULT_BACKEND_ARM64_IMAGE = 'docker.io/kontena/pharos-default-backend-arm64:0.0.2'.freeze
+      DEFAULT_BACKEND_IMAGE = 'docker.io/kontena/pharos-default-backend:0.0.2'.freeze
+
+      def image_name
+        return config.default_backend[:image] if config.default_backend&.dig(:image)
+
+        if host.cpu_arch.name == 'arm64'
+          DEFAULT_BACKEND_ARM64_IMAGE
+        else
+          DEFAULT_BACKEND_IMAGE
+        end
+      end
 
       def install
         apply_stack(
           configmap: config.configmap || {},
-          node_selector: config.node_selector
+          node_selector: config.node_selector,
+          image: self.image_name
         )
       end
     end
