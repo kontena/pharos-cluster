d6510cd4496290e07beb5016335ea7bf63273da4
Kimmo Lehto <kimmo.lehto@gmail.com>
2018-04-03 10:15:10 +0300

    Store cluster.yml to configmap (#158)

--- lib/kupo/up_command.rb
+++ lib/kupo/up_command.rb
@@ -139,6 +139,7 @@ module Kupo
       Phases::ConfigureNetwork.new(master, config.network).call
       Phases::ConfigureMetrics.new(master).call
       Phases::LabelNode.new(master, master).call
+      Phases::StoreClusterYAML.new(master, config_content).call
     end
 
     # @param master [Kupo::Configuration::Node]
