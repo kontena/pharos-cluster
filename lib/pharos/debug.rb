befa6747d0ba543b44629ac41724f6616d184f3e
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-04-04 16:47:46 +0300

    Pharos::Logging will bring the .debug? with it

--- lib/pharos/debug.rb
+++ lib/pharos/debug.rb
@@ -1,37 +1,35 @@
 # frozen_string_literal: true
 
-require 'logger'
-
 module Pharos
   module Debug
-    def self.module
-      ENV['DEBUG'].to_s.empty? ? Disabled : Enabled
+    def self.module(env_key = 'DEBUG')
+      ENV[env_key].to_s.empty? ? Disabled : Enabled
     end
 
     module Disabled
-      LOG_LEVEL = Logger::INFO
+      LOG_LEVEL = :INFO
+
+      private
 
       def debug?
         false
       end
-      module_function :debug?
 
-      def debug; end
-      module_function :debug
+      def if_debug; end
     end
 
     module Enabled
-      LOG_LEVEL = Logger::DEBUG
+      LOG_LEVEL = :DEBUG
+
+      private
 
       def debug?
         true
       end
-      module_function :debug?
 
-      def debug
+      def if_debug
         yield
       end
-      module_function :debug
     end
   end
 end
--- lib/pharos/logging.rb
+++ lib/pharos/logging.rb
@@ -3,31 +3,35 @@
 require 'logger'
 
 module Pharos
-  module Phases
-    module Logging
-      def self.logger
-        @logger
-      end
+  module Logging
+    include Pharos::Debug.module
 
-      def self.logger=(log)
-        @logger = (log ? log : Logger.new('/dev/null'))
-      end
+    def self.logger
+      @logger
+    end
 
-      def logger
-        Pharos::Phases::Logging.logger
-      end
+    def self.logger=(log)
+      @logger = log || Logger.new('/dev/null')
+    end
 
-      def self.__init__
-        @logger = Logger.new($stdout).tap do |logger|
-          logger.progname = 'API'
-          logger.level = Pharos::Debug.module.const_get(:LOG_LEVEL)
-          logger.formatter = proc do |_severity, _datetime, _progname, msg|
-            "    #{msg}\n"
-          end
+    FORMATTER = "    %s\n"
+
+    def self.__init__
+      Pharos::Logging.logger = Logger.new(ENV["PHAROS_LOG"].to_s.empty? ? $stdout : ENV["PHAROS_LOG"]).tap do |logger|
+        logger.progname = 'API'
+        logger.level = Logger.const_get(LOG_LEVEL)
+        logger.formatter = proc do |_severity, _datetime, _progname, msg|
+          FORMATTER % msg
         end
       end
+    end
 
-      __init__ unless defined?(@logger)
+    __init__ unless defined?(@logger)
+
+    private
+
+    def logger
+      Pharos::Logging.logger
     end
   end
 end
