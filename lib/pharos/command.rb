0fdf56c2975eaf719a6357ec36b00ad10794c5f4
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-04-06 12:11:24 +0300

    Enable common global optflags for -v/--version and -d/--debug

--- lib/pharos/command.rb
+++ lib/pharos/command.rb
@@ -2,6 +2,15 @@
 
 module Pharos
   class Command < Clamp::Command
+    option ['-v', '--version'], :flag, "print pharos-cluster version" do
+      puts "pharos-cluster #{Pharos::VERSION}"
+      exit 0
+    end
+
+    option ['-d', '--debug'], :flag, "enable debug output", environment_variable: "DEBUG" do
+      ENV["DEBUG"] = "true"
+    end
+
     def pastel
       @pastel ||= Pastel.new
     end
