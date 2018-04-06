bc18d0aad85100c6bbbffa9a204c4bb426223553
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-04-06 11:20:44 +0300

    Move error handling to Pharos::Command

--- lib/pharos/command.rb
+++ lib/pharos/command.rb
@@ -2,6 +2,15 @@
 
 module Pharos
   class Command < Clamp::Command
+    include Pharos::Logging
+
+    def run(*args)
+      super
+    rescue StandardError => ex
+      raise if ex.class.to_s["Clamp::"] || debug?
+      abort "ERROR: #{ex.message} (#{ex.class})"
+    end
+
     def pastel
       @pastel ||= Pastel.new
     end
