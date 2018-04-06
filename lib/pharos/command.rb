064c79f9654ee45d58a6d88c87c1f681b905f112
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-04-06 11:53:43 +0300

    Add global option for toggle/force

--- lib/pharos/command.rb
+++ lib/pharos/command.rb
@@ -2,8 +2,11 @@
 
 module Pharos
   class Command < Clamp::Command
+
+    option '--[no-]color', :flag, "Colorize output", default: $stdout.tty?
+
     def pastel
-      @pastel ||= Pastel.new(enabled: $stdout.tty?)
+      @pastel ||= Pastel.new(enabled: color?)
     end
   end
 end
