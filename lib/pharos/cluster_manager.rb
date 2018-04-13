fcbb9a9b2c5e11bf50d5e20c870b327269a6ff85
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-04-13 14:54:12 +0300

    fix double configure master

--- lib/pharos/cluster_manager.rb
+++ lib/pharos/cluster_manager.rb
@@ -52,7 +52,6 @@ module Pharos
       apply_phase(Phases::ConfigureEtcd, config.etcd_hosts, ssh: true, parallel: true)
 
       apply_phase(Phases::ConfigureMaster, config.master_hosts, ssh: true, parallel: false, master: config.master_host)
-      apply_phase(Phases::ConfigureMaster, config.master_hosts, ssh: true, parallel: false, master: config.master_host)
       apply_phase(Phases::ConfigureKubelet, config.worker_hosts, ssh: true, parallel: true) # TODO: also run this phase in parallel for the master nodes, if not doing an upgrade?
       apply_phase(Phases::ConfigureClient, [config.master_host], ssh: true, parallel: false)
 
