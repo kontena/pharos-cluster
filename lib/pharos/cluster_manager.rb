a8eae62892138afd4096e0a2a4329417075b543e
Tero Marttila <tero.marttila@kontena.io>
2018-04-19 17:27:49 +0300

    fix MigrateWorker to be independent of the @master address

--- lib/pharos/cluster_manager.rb
+++ lib/pharos/cluster_manager.rb
@@ -50,7 +50,7 @@ module Pharos
 
       apply_phase(Phases::ConfigureSecretsEncryption, config.master_hosts, ssh: true, parallel: false)
       apply_phase(Phases::ConfigureMaster, config.master_hosts, ssh: true, parallel: false)
-      apply_phase(Phases::MigrateWorker, config.worker_hosts, ssh: true, parallel: true, master: config.master_host)
+      apply_phase(Phases::MigrateWorker, config.worker_hosts, ssh: true, parallel: true)
       apply_phase(Phases::ConfigureKubelet, config.worker_hosts, ssh: true, parallel: true) # TODO: also run this phase in parallel for the master nodes, if not doing an upgrade?
       apply_phase(Phases::ConfigureClient, [config.master_host], ssh: true, parallel: false)
 
