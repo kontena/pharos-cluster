3ff24ea2d9d4068d8076ab48f03df41cbf6d8f54
Tero Marttila <tero.marttila@kontena.io>
2018-04-20 11:58:06 +0300

    spec stack prune

--- spec/pharos/kube/stack_spec.rb
+++ spec/pharos/kube/stack_spec.rb
@@ -1,11 +1,6 @@
 describe Pharos::Kube::Stack do
   let(:session) { instance_double(Pharos::Kube::Session) }
-  let(:client) { instance_double(Pharos::Kube::Client) }
   subject { described_class.new(session, 'ingress-nginx') }
-
-  before do
-    allow(session).to receive(:client).and_return(client)
-  end
 
   let(:vars) { { default_backend: double(image: 'foo'), configmap: {}, node_selector: {}, arch: double(name: 'foo'), version: '1', image: 'foo'} }
 
@@ -17,11 +12,15 @@ describe Pharos::Kube::Stack do
     end
   end
 
-  describe '#resources' do
+  describe '#load_resources' do
+    let(:client) { instance_double(Pharos::Kube::Client) }
+
     before do
       allow(session).to receive(:resource) do |data|
         Pharos::Kube::Resource.new(session, data)
       end
+
+      allow(session).to receive(:client).and_return(client)
     end
 
     it 'returns a list of resources' do
@@ -48,6 +47,37 @@ describe Pharos::Kube::Stack do
       expect(subject).to receive(:prune).with(random_checksum)
 
       subject.apply(vars)
+    end
+  end
+
+  describe '#prune' do
+    let(:api_groups) { [
+      double(preferredVersion: double(groupVersion: 'test/v1'))
+    ] }
+    let(:api_client) { instance_double(Pharos::Kube::Client) }
+    let(:api_entities) { {
+      'Test' => double(resource_name: 'test')
+    } }
+    let(:resource1) { double(:resource1, metadata: OpenStruct.new(annotations: { 'pharos.kontena.io/stack-checksum' => '41' })) }
+    let(:resource2) { double(:resource1, metadata: OpenStruct.new(annotations: { 'pharos.kontena.io/stack-checksum' => '42' })) }
+    let(:api_resources) { [resource1, resource2] }
+
+    before do
+      allow(session).to receive(:api_groups).and_return(api_groups)
+      allow(session).to receive(:client).with('test/v1').and_return(api_client)
+      allow(api_client).to receive(:entities).and_return(api_entities)
+      allow(api_client).to receive(:get_entities).with('Test', 'test', label_selector: 'pharos.kontena.io/stack=ingress-nginx').and_return(api_resources)
+      allow(session).to receive(:resource) do |resource_double| resource_double end
+
+      api_resources.each do |resource|
+        allow(resource).to receive(:apiVersion=)
+      end
+    end
+
+    it "deletes the resource with the wrong checksum" do
+      expect(resource1).to receive(:delete)
+
+      subject.prune('42')
     end
   end
 end
