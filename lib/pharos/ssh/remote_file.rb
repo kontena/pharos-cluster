eeef685f92742ce3c10e922056edfa0e0ebb6b5b
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-04-23 15:58:39 +0300

    Avoid false-negatives when checking if remote host file exists

--- lib/pharos/ssh/remote_file.rb
+++ lib/pharos/ssh/remote_file.rb
@@ -53,7 +53,7 @@ module Pharos
       # True if the file exists. Assumes a bash-like shell.
       # @return [Boolean]
       def exist?
-        @client.exec?("[ -e #{escaped_path} ]")
+        @client.exec!("(sudo true || exit 1) && (sudo test -e #{escaped_path} && echo true || exit 0)").strip == "true"
       end
 
       # Performs the block if the remote file exists, otherwise returns false
