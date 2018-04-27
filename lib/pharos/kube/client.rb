f5ebb0c2b7a214b88072fe0855a558047ed6f949
Tero Marttila <SpComb@users.noreply.github.com>
2018-04-27 12:27:15 +0300

    Fix stack prune (#326)

--- lib/pharos/kube/client.rb
+++ lib/pharos/kube/client.rb
@@ -71,6 +71,22 @@ module Pharos
 
         get_entity(definition.resource_name, resource.metadata.name, resource.metadata.namespace)
       end
+
+      # @param resource [Kubeclient::Resource]
+      def delete_resource(resource, propagation_policy: 'Foreground')
+        definition = entity_for_resource(resource)
+
+        delete_entity(
+          definition.resource_name,
+          resource.metadata.name,
+          resource.metadata.namespace,
+          delete_options: {
+            apiVersion: 'v1',
+            kind: 'DeleteOptions',
+            propagationPolicy: propagation_policy
+          }
+        )
+      end
     end
   end
 end
--- spec/pharos/kube/stack_spec.rb
+++ spec/pharos/kube/stack_spec.rb
@@ -19,15 +19,61 @@ describe Pharos::Kube::Stack do
   end
 
   describe '#apply' do
-    let(:session) { double(api_groups: []) }
+    let(:resource1) { double(:resource1, metadata: OpenStruct.new) }
+    let(:resources) { [resource1] }
+    let(:random_checksum) { '42' }
 
     before do
-      allow(session).to receive(:resource).with(an_instance_of(Hash)).at_least(:once).and_return(resource)
+      allow(subject).to receive(:resources).and_return(resources)
+      allow(subject).to receive(:random_checksum).and_return(random_checksum)
     end
 
     it 'applies all resources' do
-      expect(resource).to receive(:apply)
+      expect(resource1).to receive(:apply) do
+        expect(resource1.metadata.labels['pharos.kontena.io/stack']).to eq 'ingress-nginx'
+        expect(resource1.metadata.annotations['pharos.kontena.io/stack-checksum']).to eq random_checksum
+      end
+
+      expect(subject).to receive(:prune).with(random_checksum)
+
       subject.apply
+    end
+  end
+
+  describe '#prune' do
+    let(:api_versions) { [ 'test/v1' ] }
+    let(:api_client) { instance_double(Pharos::Kube::Client) }
+    let(:api_entities) { {
+      'test' => double(entity_type: 'Test', resource_name: 'test')
+    } }
+    let(:resource1) { double(:resource1, metadata: OpenStruct.new(annotations: { 'pharos.kontena.io/stack-checksum' => '41' })) }
+    let(:resource2) { double(:resource1, metadata: OpenStruct.new(annotations: { 'pharos.kontena.io/stack-checksum' => '42' })) }
+    let(:api_resources) { [resource1, resource2] }
+
+    before do
+      allow(session).to receive(:api_versions).and_return(api_versions)
+      allow(session).to receive(:resource_client).with('test/v1').and_return(api_client)
+      allow(api_client).to receive(:entities).and_return(api_entities)
+      allow(api_client).to receive(:get_entities).with('Test', 'test', label_selector: 'pharos.kontena.io/stack=ingress-nginx').and_return(api_resources)
+      allow(session).to receive(:resource) do |resource_double| resource_double end
+
+      api_resources.each do |resource|
+        allow(resource).to receive(:apiVersion=).with('test/v1')
+        allow(resource).to receive(:kind=).with('Test')
+      end
+    end
+
+    it "deletes the resource with the wrong checksum" do
+      expect(resource1).to receive(:delete)
+
+      subject.prune('42')
+    end
+
+    it "deletes all resources with a mock checksum" do
+      expect(resource1).to receive(:delete)
+      expect(resource2).to receive(:delete)
+
+      subject.prune('-')
     end
   end
 end
