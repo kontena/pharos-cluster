0bc43d6e778710ef610cb98497bc31ab77c8704b
Jussi Nummelin <jussi@kontena.io>
2018-05-03 15:33:18 +0300

    re-order resources and fix namespacing

--- lib/pharos/addons/open_ebs.rb
+++ lib/pharos/addons/open_ebs.rb
@@ -11,13 +11,17 @@ module Pharos
         attribute :default_replicas, Pharos::Types::Int
         attribute :default_capacity, Pharos::Types::String
         attribute :default_class, Pharos::Types::Bool
+        attribute :default_storage_path, Pharos::Types::String
       }
 
       schema {
         optional(:default_replicas).filled(:int?)
         optional(:default_capacity).filled(:str?)
         optional(:default_class).filled(:bool?)
+        optional(:default_storage_path).filled(:str?)
       }
+
+      DEFAULT_STORAGE_PATH = '/var/openebs'.freeze
 
       def validate
         super
@@ -32,7 +36,8 @@ module Pharos
         apply_stack(
           default_replicas: config.default_replicas || default_replica_count,
           default_capacity: config.default_capacity || '5G',
-          is_default_class: config.default_class == true
+          is_default_class: config.default_class == true,
+          default_storage_pool_path: config.default_storage_path || DEFAULT_STORAGE_PATH
         )
       end
     end
