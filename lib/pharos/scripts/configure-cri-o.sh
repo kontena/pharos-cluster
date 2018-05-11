7a67fc7334f167e317baa7d49b90832c08257f48
Jussi Nummelin <jussi@kontena.io>
2018-05-11 09:30:07 +0300

    validate crictl binary download

--- lib/pharos/scripts/configure-cri-o.sh
+++ lib/pharos/scripts/configure-cri-o.sh
@@ -16,6 +16,8 @@ rm /etc/cni/net.d/100-crio-bridge.conf /etc/cni/net.d/200-loopback.conf || true
 systemctl start crio
 
 # Install crictl binary
-curl -sSL https://github.com/kubernetes-incubator/cri-tools/releases/download/v1.0.0-beta.0/crictl-v1.0.0-beta.0-linux-amd64.tar.gz | tar xz
-
+CRICTL_DOWNLOAD_SHA="597a4db0289870d81d0377396ddaf4c23725a47b33b30856e6291c2b958786f3"
+curl -sSL https://github.com/kubernetes-incubator/cri-tools/releases/download/v1.0.0-beta.0/crictl-v1.0.0-beta.0-linux-${CPU_ARCH}.tar.gz -o /tmp/crictl.tar.gz
+echo "$CRICTL_DOWNLOAD_SHA  /tmp/crictl.tar.gz" | shasum -a256 -c
+tar xzf /tmp/crictl.tar.gz
 install -m 755 -o root -g root crictl /usr/bin/crictl && rm crictl
