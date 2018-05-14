bb52483e7563b8cdd1baa85519fb7b72df60b43b
Tero Marttila <tero.marttila@kontena.io>
2018-05-14 16:29:50 +0300

    simplify diff

--- lib/pharos/cluster_manager.rb
+++ lib/pharos/cluster_manager.rb
@@ -77,7 +77,7 @@ module Pharos
       apply_phase(Phases::SetupMaster, master_hosts, ssh: true, parallel: true)
       apply_phase(Phases::UpgradeMaster, master_hosts, ssh: true, parallel: false)
 
-      apply_phase(Phases::MigrateWorker, config.worker_hosts, ssh: true, parallel: true)
+      apply_phase(Phases::MigrateWorker, config.worker_hosts, ssh: true, parallel: true, master: master_hosts.first)
       apply_phase(Phases::ConfigureKubelet, config.hosts, ssh: true, parallel: true)
 
       apply_phase(Phases::ConfigureMaster, master_hosts, ssh: true, parallel: false)
