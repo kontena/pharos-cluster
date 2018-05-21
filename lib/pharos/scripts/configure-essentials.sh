bc6a564ce6755a5c10a94b8a71c37fd6ec34b2d3
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-05-21 14:34:21 +0300

    always disable apt unattended-upgrade

--- lib/pharos/scripts/configure-essentials.sh
+++ lib/pharos/scripts/configure-essentials.sh
@@ -7,3 +7,8 @@ if ! dpkg -l apt-transport-https software-properties-common > /dev/null; then
     apt-get update -y
     apt-get install -y apt-transport-https software-properties-common
 fi
+
+cat <<EOF >/etc/apt/apt.conf.d/20auto-upgrades
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "0";
+EOF
