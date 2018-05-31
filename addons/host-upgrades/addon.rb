48796230ecefd596b4f7c01ee31911350ed0370d
Tero Marttila <tero.marttila@kontena.io>
2018-05-31 16:05:50 +0300

    replace host-upgrades addon with pharos-host-upgrades

--- addons/host-upgrades/addon.rb
+++ addons/host-upgrades/addon.rb
@@ -1,20 +1,27 @@
 # frozen_string_literal: true
 
 Pharos.addon 'host-upgrades' do
-  version '0.1.0'
+  version '0.0.0'
   license 'Apache License 2.0'
 
-  config {
-    attribute :interval, Pharos::Types::String
-  }
+  IMAGE = 'quay.io/kontena/pharos-host-upgrades'
 
-  config_schema {
-    required(:interval).filled(:str?, :duration?)
+  config {
+    attribute :image_tag, Pharos::Types::String.default("edge")
+    attribute :schedule, Pharos::Types::String.default("0 0 0 * * *")
+    attribute :schedule_window, Pharos::Types::String.default("2h")
+    attribute :reboot, Pharos::Types::Bool.default(false)
+    attribute :drain, Pharos::Types::Bool.default(true)
   }
 
   install {
     apply_resources(
-      interval: duration.parse(config.interval).to_sec
+      image: "#{IMAGE}:#{config.image_tag}",
+      schedule: config.schedule,
+      schedule_window: config.schedule_window,
+      reboot: config.reboot,
+      drain: config.drain,
+      journal: false, # disabled ue to https://github.com/kontena/pharos-host-upgrades/issues/15
     )
   }
 end
