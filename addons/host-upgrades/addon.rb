eaa5946b315b72d545cd4d9573419ca255014239
Tero Marttila <tero.marttila@kontena.io>
2018-06-01 14:25:51 +0300

    make schedule required

--- addons/host-upgrades/addon.rb
+++ addons/host-upgrades/addon.rb
@@ -6,10 +6,14 @@ Pharos.addon 'host-upgrades' do
 
   config {
     attribute :image_tag, Pharos::Types::String.default("edge")
-    attribute :schedule, Pharos::Types::String.default("0 0 0 * * *")
+    attribute :schedule, Pharos::Types::String
     attribute :schedule_window, Pharos::Types::String
     attribute :reboot, Pharos::Types::Bool.default(false)
     attribute :drain, Pharos::Types::Bool.default(true)
+  }
+
+  config_schema {
+    required(:schedule).filled(:str?)
   }
 
   install {
