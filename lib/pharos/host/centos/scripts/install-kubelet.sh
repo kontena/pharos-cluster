ed4e06e34547907bb77a979dd23c13683a4fff08
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-06-06 11:08:07 +0300

    more yummy

--- lib/pharos/host/centos/scripts/install-kubelet.sh
+++ lib/pharos/host/centos/scripts/install-kubelet.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-apt-get install -y kubelet-${KUBE_VERSION} kubectl-${KUBE_VERSION} kubeadm-${KUBEADM_VERSION}
+yum install -y kubelet-${KUBE_VERSION} kubectl-${KUBE_VERSION} kubeadm-${KUBEADM_VERSION}
 
 if systemctl is-active --quiet rpcbind; then
     systemctl stop rpcbind
