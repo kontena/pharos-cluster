981e2c4fbef44e622af39bce2a305b93e3af590f
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-06-12 14:02:31 +0300

    Handle migration from 1.1.x to 1.2.x (#424)

--- lib/pharos/scripts/wait-kubelet-proxy.sh
+++ lib/pharos/scripts/wait-kubelet-proxy.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Waiting kubelet-proxy to launch on port 6443..."
 
-while ! (echo > /dev/tcp/localhost/6443) >/dev/null 2>&1; do
+while ! ( bash -c 'echo > /dev/tcp/localhost/6443') >/dev/null 2>&1; do
   sleep 1;
 done
 
