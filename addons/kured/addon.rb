ba9e056f8c20d351127c45c933da0545433f0aa3
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-06-20 14:14:12 +0300

    show deprecation notice (#442)

--- addons/kured/addon.rb
+++ addons/kured/addon.rb
@@ -3,4 +3,10 @@
 Pharos.addon 'kured' do
   version 'master-b27aaa1'
   license 'Apache License 2.0'
+
+  install {
+    logger.warn { '~~~> kured is deprecated!' }
+    logger.warn { '~~~> see https://www.pharos.sh/docs/addons/kured.html' }
+    apply_resources
+  }
 end
