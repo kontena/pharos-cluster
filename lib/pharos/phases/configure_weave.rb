5076e8912bb23653a515f3b81c6d3e1b989c2df0
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-07-30 17:48:10 +0300

    update weave net to 2.4.0

--- lib/pharos/phases/configure_weave.rb
+++ lib/pharos/phases/configure_weave.rb
@@ -5,7 +5,7 @@ module Pharos
     class ConfigureWeave < Pharos::Phase
       title "Configure Weave network"
 
-      WEAVE_VERSION = '2.3.0'
+      WEAVE_VERSION = '2.4.0'
 
       register_component(
         name: 'weave-net', version: WEAVE_VERSION, license: 'Apache License 2.0',
