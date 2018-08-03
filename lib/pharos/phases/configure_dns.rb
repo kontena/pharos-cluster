61157a4c050eae89d8488af3111714bb93c1d9ae
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-08-03 10:07:41 +0300

    patch coredns image

--- lib/pharos/phases/configure_dns.rb
+++ lib/pharos/phases/configure_dns.rb
@@ -49,47 +49,50 @@ module Pharos
       def patch_deployment(name, replicas:, max_surge:, max_unavailable:)
         logger.info { "Patching #{name} deployment with #{replicas} replicas (max-surge #{max_surge}, max-unavailable #{max_unavailable})..." }
 
-        resource = Pharos::Kube.session(@master.api_address).resource(
-          apiVersion: 'extensions/v1beta1',
-          kind: 'Deployment',
-          metadata: {
-            namespace: 'kube-system',
-            name: name
-          },
-          spec: {
-            replicas: replicas,
-            strategy: {
-              type: "RollingUpdate",
-              rollingUpdate: {
-                maxSurge: max_surge, # must be zero for a two-node cluster
-                maxUnavailable: max_unavailable, # must be at least one, even for a single-node cluster
-              }
-            },
-            template: {
-              spec: {
-                affinity: {
-                  podAntiAffinity: {
-                    requiredDuringSchedulingIgnoredDuringExecution: [
-                      {
-                        labelSelector: {
-                          matchExpressions: [
-                            {
-                              key: "k8s-app",
-                              operator: "In",
-                              values: [name]
-                            }
-                          ]
-                        },
-                        topologyKey: "kubernetes.io/hostname"
-                      }
-                    ]
-                  }
+        Pharos::Kube.session(@master.api_address).resource_client('apps/v1').patch_deployment(
+          name,
+          {
+            spec: {
+              replicas: replicas,
+              strategy: {
+                type: "RollingUpdate",
+                rollingUpdate: {
+                  maxSurge: max_surge, # must be zero for a two-node cluster
+                  maxUnavailable: max_unavailable, # must be at least one, even for a single-node cluster
+                }
+              },
+              template: {
+                spec: {
+                  affinity: {
+                    podAntiAffinity: {
+                      requiredDuringSchedulingIgnoredDuringExecution: [
+                        {
+                          labelSelector: {
+                            matchExpressions: [
+                              {
+                                key: "k8s-app",
+                                operator: "In",
+                                values: [name]
+                              }
+                            ]
+                          },
+                          topologyKey: "kubernetes.io/hostname"
+                        }
+                      ]
+                    }
+                  },
+                  containers: [
+                    {
+                      name: 'coredns',
+                      image: "#{@config.image_repository}/coredns-#{@host.cpu_arch.name}:#{Pharos::COREDNS_VERSION}"
+                    }
+                  ]
                 }
               }
             }
-          }
+          },
+          'kube-system'
         )
-        resource.update
       end
     end
   end
