9cd4b663e186bf4d64e9f6dc6d83b3bdacb247e1
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-08-06 11:44:23 +0300

    add gpg key in upgrade-kubeadm

--- lib/pharos/host/el7/scripts/upgrade-kubeadm.sh
+++ lib/pharos/host/el7/scripts/upgrade-kubeadm.sh
@@ -10,8 +10,9 @@ fi
 
 BIN_URL="https://dl.bintray.com/kontena/pharos-bin/kube/${VERSION}/kubeadm-${ARCH}.gz"
 
+curl -fsSL https://bintray.com/user/downloadSubjectPublicKey?username=bintray | gpg --import
 curl -fsSL $BIN_URL -o /tmp/kubeadm.gz
-curl -fsSL "${BIN_URL}.asc" -o tmp/kubeadm.gz.asc
+curl -fsSL "${BIN_URL}.asc" -o /tmp/kubeadm.gz.asc
 gpg --verify /tmp/kubeadm.gz.asc /tmp/kubeadm.gz
 gunzip /tmp/kubeadm.gz
 install -o root -g root -m 0755 -T /tmp/kubeadm /usr/local/bin/pharos-kubeadm-${VERSION}
