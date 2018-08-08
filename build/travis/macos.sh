3aa796f5b767556f1199cb68620b25dfa69279a8
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-08-08 09:28:49 +0300

    Generate sha256sums on release and add to github release page

--- build/travis/macos.sh
+++ build/travis/macos.sh
@@ -5,13 +5,15 @@ set -ue
 brew install squashfs
 curl -sL https://dl.bintray.com/kontena/ruby-packer/0.5.0-dev/rubyc-darwin-amd64.gz | gunzip > /usr/local/bin/rubyc
 chmod +x /usr/local/bin/rubyc
-rubyc -o pharos-cluster pharos-cluster
-./pharos-cluster version
+rubyc -o pharos-cluster-darwin-amd64 pharos-cluster
+./pharos-cluster-darwin-amd64 version
+shasum -a 256 pharos-cluster-darwin-amd64 > pharos-cluster-darwin-amd64.sha256
 
 # ship to github
 curl -sL https://github.com/aktau/github-release/releases/download/v0.7.2/darwin-amd64-github-release.tar.bz2 | tar -xjO > /usr/local/bin/github-release
 chmod +x /usr/local/bin/github-release
-/usr/local/bin/github-release upload \
+ls -1 pharos-cluster-darwin-adm64 pharos-cluster-darwin-amd64.sha256 | xargs -n1 -I{} -P0 -- \
+  /usr/local/bin/github-release upload \
     --user kontena \
     --repo pharos-cluster \
     --tag $TRAVIS_TAG \
