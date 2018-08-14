53334c35e37cf2932a7d89f79a31f2ac276f1d77
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-08-14 10:19:47 +0300

    Use UpCommand as base to avoid duplication

--- lib/pharos/kubeconfig_command.rb
+++ lib/pharos/kubeconfig_command.rb
@@ -1,25 +1,15 @@
 # frozen_string_literal: true
 
+require 'pharos/up_command'
 require 'pharos/ssh/client'
 require 'pharos/yaml_file'
 require 'pharos/terraform/json_parser'
 require 'pharos/kube/config'
 
 module Pharos
-  class KubeconfigCommand < Pharos::Command
-    option ['-c', '--config'], 'PATH', 'Path to config file (default: cluster.yml)', attribute_name: :config_yaml
-
-    option '--tf-json', 'PATH', 'Path to terraform output json' do |config_path|
-      begin
-        File.realpath(config_path)
-      rescue Errno::ENOENT
-        signal_usage_error 'File does not exist: %<path>s' % { path: config_path }
-      end
-    end
-
+  class KubeconfigCommand < UpCommand
     option ['-n', '--name'], 'NAME', 'Overwrite cluster name', attribute_name: :new_name
     option ['-C', '--context'], 'CONTEXT', 'Overwrite context name', attribute_name: :new_context
-
     option ['-m', '--merge'], '[FILE]', 'Merge with existing configuration file', multivalued: true
 
     REMOTE_FILE = "/etc/kubernetes/admin.conf"
@@ -38,21 +28,6 @@ module Pharos
 
     private
 
-    # @return [Pharos::YamlFile]
-    def cluster_config
-      if config_yaml
-        begin
-          Pharos::YamlFile.new(File.realpath(config_yaml))
-        rescue Errno::ENOENT
-          signal_usage_error 'File does not exist: %<path>s' % { path: config_yaml }
-        end
-      else
-        cluster_config = Dir.glob('cluster.{yml,yml.erb}').first
-        signal_usage_error 'File does not exist: cluster.yml' if cluster_config.nil?
-        Pharos::YamlFile.new(cluster_config)
-      end
-    end
-
     def config_file_content
       file = ssh.file(REMOTE_FILE)
       signal_usage_error "Remote file #{REMOTE_FILE} not found" unless file.exist?
@@ -68,24 +43,7 @@ module Pharos
 
     # @return [Pharos::Config]
     def master_host
-      return @master_host if @master_host
-      config_hash = cluster_config.load(ENV.to_h)
-      load_terraform(tf_json, config_hash) if tf_json
-      config = Pharos::Config.load(config_hash)
-      signal_usage_error 'No master hosts defined' if config.master_hosts.empty?
-      @master_host = config.master_host
-    end
-
-    # @param file [String]
-    # @param config [Hash]
-    # @return [Hash]
-    def load_terraform(file, config)
-      tf_parser = Pharos::Terraform::JsonParser.new(File.read(file))
-      config['hosts'] ||= []
-      config['api'] ||= {}
-      config['hosts'] += tf_parser.hosts
-      config['api'].merge!(tf_parser.api) if tf_parser.api
-      config
+      @master_host ||= load_config(quiet: true).master_host
     end
   end
 end
