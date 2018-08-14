de48f3769588b177bbdc37088cf4fbdd8448c2ce
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-08-14 20:55:29 +0300

    upload binaries to s3

--- build/drone/ubuntu_xenial.sh
+++ build/drone/ubuntu_xenial.sh
@@ -8,8 +8,10 @@ apt-get install -y -q squashfs-tools build-essential ruby bison ruby-dev git-cor
 curl -sL https://dl.bintray.com/kontena/ruby-packer/0.5.0-dev/rubyc-linux-amd64.gz | gunzip > /usr/local/bin/rubyc
 chmod +x /usr/local/bin/rubyc
 gem install bundler
-rubyc -o pharos-cluster pharos-cluster
-./pharos-cluster version
+version=${DRONE_TAG#"v"}
+package="pharos-cluster-linux-amd64-${version}"
+rubyc -o $package pharos-cluster
+./$package version
 
 # ship to github
 curl -sL https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2 | tar -xjO > /usr/local/bin/github-release
@@ -18,5 +20,5 @@ chmod +x /usr/local/bin/github-release
     --user kontena \
     --repo pharos-cluster \
     --tag $DRONE_TAG \
-    --name "pharos-cluster-linux-amd64" \
-    --file ./pharos-cluster
+    --name $package \
+    --file ./$package
--- build/travis/macos.sh
+++ build/travis/macos.sh
@@ -5,8 +5,10 @@ set -ue
 brew install squashfs
 curl -sL https://dl.bintray.com/kontena/ruby-packer/0.5.0-dev/rubyc-darwin-amd64.gz | gunzip > /usr/local/bin/rubyc
 chmod +x /usr/local/bin/rubyc
-rubyc -o pharos-cluster pharos-cluster
-./pharos-cluster version
+version=${TRAVIS_TAG#"v"}
+package="pharos-cluster-linux-amd64-${version}"
+rubyc -o $package pharos-cluster
+./$package version
 
 # ship to github
 curl -sL https://github.com/aktau/github-release/releases/download/v0.7.2/darwin-amd64-github-release.tar.bz2 | tar -xjO > /usr/local/bin/github-release
@@ -15,5 +17,8 @@ chmod +x /usr/local/bin/github-release
     --user kontena \
     --repo pharos-cluster \
     --tag $TRAVIS_TAG \
-    --name "pharos-cluster-darwin-amd64" \
-    --file ./pharos-cluster
+    --name $package \
+    --file ./$package
+
+mkdir -p upload
+mv $package upload/
