43b167f60b27c93548d2ce46eb994d27a197d618
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-08-17 10:02:53 +0300

    rubocop laws...

--- lib/pharos/up_command.rb
+++ lib/pharos/up_command.rb
@@ -111,7 +111,7 @@ module Pharos
 
       craft_time = Time.now - start_time
       defined_opts = ARGV[1..-1].join(" ")
-      defined_opts = defined_opts + " " unless defined_opts.empty?
+      defined_opts += " " unless defined_opts.empty?
       puts pastel.green("==> Cluster has been crafted! (took #{humanize_duration(craft_time.to_i)})")
       puts "    To configure kubectl for connecting to the cluster, use:"
       puts "      #{$PROGRAM_NAME} kubeconfig #{defined_opts}> kubeconfig"
