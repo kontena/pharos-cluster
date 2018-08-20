b53c332ceeaea868d7734ba2bf713ecc2fd8f867
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-08-20 10:08:33 +0300

    Dont show the full path to pharos-cluster in post-install command example

--- lib/pharos/up_command.rb
+++ lib/pharos/up_command.rb
@@ -111,7 +111,7 @@ module Pharos
       craft_time = Time.now - start_time
       puts pastel.green("==> Cluster has been crafted! (took #{humanize_duration(craft_time.to_i)})")
       puts "    To configure kubectl for connecting to the cluster, use:"
-      puts "      #{$PROGRAM_NAME} kubeconfig > $HOME/.pharos/config"
+      puts "      #{File.basename($PROGRAM_NAME)} kubeconfig > $HOME/.pharos/config"
       puts "      export KUBECONFIG=$KUBECONFIG:$HOME/.pharos/config"
 
       manager.disconnect
