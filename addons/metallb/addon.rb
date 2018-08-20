9d648e76046e4168ac7a392f820b6237d63c7ffe
Jussi Nummelin <jussi@kontena.io>
2018-08-20 20:21:14 +0300

    initial MetalLB addon implementation

---
+++ addons/metallb/addon.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+Pharos.addon 'metal-lb' do
+
+  version '0.7.3'
+  license 'Apache License 2.0'
+
+  config {
+    attribute :address_pools, Pharos::Types::Array
+    attribute :peers, Pharos::Types::Array
+  }
+
+  config_schema {
+    required(:address_pools).filled(min_size?: 1) do
+      each do
+        schema do
+          required(:name).filled(:str?)
+          required(:protocol).filled(:str?)
+          required(:addresses).each(:str?)
+        end
+      end
+    end
+
+
+    optional(:peers) do
+      each do
+        schema do
+          required(:peer_address).filled(:str?)
+          required(:peer_asn).filled(:str?)
+          required(:my_asn).filled(:str?)
+          optional(:node_selectors).each(:hash?)
+        end
+      end
+    end
+  }
+
+  def validate
+    super
+    if config.address_pools.count {|pool| pool.dig(:protocol) == 'bgp'} > 0
+    raise Pharos::InvalidAddonError, "Peers have to be configured for BGP protocol" if config.peers.nil? || config.peers.empty?
+    end
+  end
+
+end
