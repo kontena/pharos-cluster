e9e83d2b81e2927073daa972fd62953f2bfb2dea
Jussi Nummelin <jussi@kontena.io>
2018-08-20 20:36:40 +0300

    make Mr. Rubocop happy

--- addons/metallb/addon.rb
+++ addons/metallb/addon.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Pharos.addon 'metal-lb' do
-
   version '0.7.3'
   license 'Apache License 2.0'
 
@@ -21,7 +20,6 @@ Pharos.addon 'metal-lb' do
       end
     end
 
-
     optional(:peers) do
       each do
         schema do
@@ -36,9 +34,8 @@ Pharos.addon 'metal-lb' do
 
   def validate
     super
-    if config.address_pools.count {|pool| pool.dig(:protocol) == 'bgp'} > 0
+    # Validate BGP peers exist if BGP used
+    return unless config.address_pools.count { |pool| pool.dig(:protocol) == 'bgp' }.positive?
     raise Pharos::InvalidAddonError, "Peers have to be configured for BGP protocol" if config.peers.nil? || config.peers.empty?
-    end
   end
-
 end
