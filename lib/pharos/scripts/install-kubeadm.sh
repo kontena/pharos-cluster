c9049af6b6382ff53f8561481c57e9c222fac7f8
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-09-05 15:58:16 +0300

    Revert "Fix found problems part 2"
    This reverts commit a0fd7b7f847c10416723e3a472f2cc7bde51408e.

--- lib/pharos/scripts/install-kubeadm.sh
+++ lib/pharos/scripts/install-kubeadm.sh
@@ -2,10 +2,12 @@
 
 set -ex
 
-[ "$(kubeadm version -o short)" = "v${VERSION}" ] && exit
+if [ $(kubeadm version -o short) = "v${VERSION}" ]; then
+    exit
+fi
 
 cd /tmp
 export DEBIAN_FRONTEND=noninteractive
-apt-get download "kubeadm=${VERSION}-00"
-dpkg -i --ignore-depends=kubelet kubeadm_"${VERSION}"*.deb
-rm -f kubeadm_"${VERSION}"*.deb
+apt-get download kubeadm=${VERSION}-00
+dpkg -i --ignore-depends=kubelet kubeadm_${VERSION}*.deb
+rm -f kubeadm_${VERSION}*.deb
