546925f4490712a1d9e3a97a93f147c9e301505b
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-09-12 20:51:55 +0300

    cri-o 1.11.3

--- lib/pharos_cluster.rb
+++ lib/pharos_cluster.rb
@@ -8,7 +8,7 @@ require_relative "pharos/error"
 require_relative "pharos/root_command"
 
 module Pharos
-  CRIO_VERSION = '1.11.2'
+  CRIO_VERSION = '1.11.3'
   KUBE_VERSION = ENV.fetch('KUBE_VERSION') { '1.11.1' }
   KUBEADM_VERSION = ENV.fetch('KUBEADM_VERSION') { KUBE_VERSION }
   ETCD_VERSION = ENV.fetch('ETCD_VERSION') { '3.2.18' }
