ba2d8699b447ac1fa35fafb464ae8d15b0f7afda
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-09-16 17:30:24 +0300

    fix config

--- addons/rook-ceph/addon.rb
+++ addons/rook-ceph/addon.rb
@@ -13,4 +13,27 @@ Pharos.addon 'rook-ceph' do
       required(:enabled).filled(:bool?)
     end
   }
+
+  install {
+    cluster = K8s::Resource.new(
+      apiVersion: 'ceph.rook.io/v1beta1',
+      kind: 'Cluster',
+      metadata: {
+        name: 'rook-ceph',
+        namespace: 'rook-ceph'
+      },
+      spec: {
+        dataDirHostPath: config.dataDirHostPath,
+        storage: config.storage,
+        placement: config.placement || {},
+        resources: config.resources || {},
+        dashboard: config.dashboard || { enabled: false }
+      }
+    )
+    apply_resources(cluster: stringify_hash(cluster.to_h))
+  }
+
+  def stringify_hash(hash)
+    JSON.load(JSON.dump(hash))
+  end
 end
