5b54460198e6f26fb573c982b4b7da179622db9e
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-09-24 13:04:21 +0300

    mkdir before using it

--- build/drone/ubuntu_oss.sh
+++ build/drone/ubuntu_oss.sh
@@ -12,5 +12,6 @@ chmod +x /usr/local/bin/rubyc
 gem install bundler
 version=${DRONE_TAG#"v"}
 package="pharos-cluster-oss-linux-amd64-${version}"
+sudo mkdir /__enclose_io_memfs__
 rubyc -o $package -d /__enclose_io_memfs__ pharos-cluster
 ./$package version
