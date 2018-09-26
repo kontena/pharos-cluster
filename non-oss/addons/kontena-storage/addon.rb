4cc8742b5b44b3865a75254e23e89454cdfb63cb
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-09-26 12:36:42 -0400

    improve addon config

--- non-oss/addons/kontena-storage/addon.rb
+++ non-oss/addons/kontena-storage/addon.rb
@@ -5,12 +5,88 @@ Pharos.addon 'kontena-storage' do
   license 'Kontena License'
 
   config_schema {
-    required(:dataDirHostPath).filled(:str?)
-    required(:storage).schema
-    optional(:placement).schema
-    optional(:resources).schema
+    required(:data_dir).filled(:str?)
+    required(:storage).schema do
+      required(:use_all_nodes).filled(:bool?)
+      optional(:device_filter).filled(:str?)
+      optional(:nodes).each do
+        schema do
+          required(:name).filled(:str?)
+          optional(:directories).each do
+            schema do
+              required(:name).filled(:str?)
+            end
+          end
+          optional(:device_filter).filled(:str?)
+          optional(:devices).each do
+            schema do
+              required(:name).filled(:str?)
+            end
+          end
+          optional(:config).schema
+          optional(:resources).schema do
+            optional(:limits).schema do
+              required(:cpu).filled(:str?)
+              required(:memory).filled(:str?)
+            end
+            optional(:requests).schema do
+              required(:cpu).filled(:str?)
+              required(:memory).filled(:str?)
+            end
+          end
+        end
+      end
+      optional(:directories).each do
+        schema do
+          required(:name).filled(:str?)
+        end
+      end
+    end
+    optional(:placement).schema do
+      optional(:all).schema
+      optional(:mgr).schema
+      optional(:mon).schema
+      optional(:osd).schema
+    end
+    optional(:resources).schema do
+      optional(:mgr).schema do
+        optional(:limits).schema do
+          required(:cpu).filled(:str?)
+          required(:memory).filled(:str?)
+        end
+        optional(:requests).schema do
+          required(:cpu).filled(:str?)
+          required(:memory).filled(:str?)
+        end
+      end
+      optional(:mon).schema do
+        optional(:limits).schema do
+          required(:cpu).filled(:str?)
+          required(:memory).filled(:str?)
+        end
+        optional(:requests).schema do
+          required(:cpu).filled(:str?)
+          required(:memory).filled(:str?)
+        end
+      end
+      optional(:osd).schema do
+        optional(:limits).schema do
+          required(:cpu).filled(:str?)
+          required(:memory).filled(:str?)
+        end
+        optional(:requests).schema do
+          required(:cpu).filled(:str?)
+          required(:memory).filled(:str?)
+        end
+      end
+    end
     optional(:dashboard).schema do
       required(:enabled).filled(:bool?)
+    end
+    optional(:pool).schema do
+      required(:replicated).schema do
+        required(:size).filled(:int?)
+      end
     end
   }
 
@@ -23,10 +99,16 @@ Pharos.addon 'kontena-storage' do
         namespace: 'kontena-storage'
       },
       spec: {
-        dataDirHostPath: config.dataDirHostPath,
-        storage: config.storage,
-        placement: config.placement || {},
-        resources: config.resources || {},
+        serviceAccount: 'kontena-storage-cluster',
+        dataDirHostPath: config.data_dir,
+        storage: {
+          useAllNodes: config.storage&.use_all_nodes || true,
+          useAllDevices: false,
+          deviceFilter: config.storage&.device_filter,
+          nodes: config.storage&.nodes&.map { |n| n.to_h.to_camelback_keys }
+        },
+        placement: (config.placement || {}).to_h.to_camelback_keys,
+        resources: (config.resources || {}).to_h.to_camelback_keys,
         dashboard: config.dashboard || { enabled: false }
       }
     )
