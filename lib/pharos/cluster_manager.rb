123bd1cf449ee5280110400eeb7073a010517cba
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-09-26 15:21:29 +0300

    Fix comment split

--- lib/pharos/cluster_manager.rb
+++ lib/pharos/cluster_manager.rb
@@ -77,9 +77,9 @@ module Pharos
 
     def apply_phases
       # we need to use sorted masters because phases expects that first one has
+      # ca etc config files
       master_hosts = sorted_master_hosts
 
-      # ca etc config files
       apply_phase(Phases::MigrateMaster, master_hosts, ssh: true, parallel: true)
       apply_phase(Phases::ConfigureHost, config.hosts, ssh: true, parallel: true)
       apply_phase(Phases::ConfigureClient, [master_hosts.first], ssh: true, master: master_hosts.first, parallel: false, optional: true)
