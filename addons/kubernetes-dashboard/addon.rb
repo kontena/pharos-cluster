7f27bf061ec220a0cd9fae26cb6edb560df33cc7
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-09-27 16:22:17 +0300

    Fix kubernetes dashboard addon first run timing issue

--- addons/kubernetes-dashboard/addon.rb
+++ addons/kubernetes-dashboard/addon.rb
@@ -9,12 +9,22 @@ Pharos.addon 'kubernetes-dashboard' do
 
     logger.info { "~~> kubernetes-dashboard can be accessed via kubectl proxy. Check proxy URL with: kubectl cluster-info" }
 
-    service_account = kube_client.api('v1').resource('serviceaccounts', namespace: 'kube-system').get('dashboard-admin')
-    token_secret = service_account.secrets[0]
-    if token_secret
+    retry_times = 0
+    begin
+      service_account = kube_client.api('v1').resource('serviceaccounts', namespace: 'kube-system').get('dashboard-admin')
+      raise "secret not available" if service_account.secrets.nil? || service_account.secrets.empty?
+      token_secret = service_account.secrets[0]
       logger.info { "~~> kubernetes-dashboard admin token can be fetched using: kubectl describe secret #{token_secret.name} -n kube-system" }
-    else
-      logger.error { "~~> kubernetes-dashboard admin token cannot be found" }
+    rescue => ex
+      raise unless ex.message == "secret not available"
+      retry_times += 1
+      if retry_times > 10
+        logger.error { "~~> kubernetes-dashboard admin token cannot be found" }
+      else
+        logger.send(retry_times == 1 ? :info : :debug) { "Waiting for secrets to update .." }
+        sleep 5
+        retry
+      end
     end
   }
 end
