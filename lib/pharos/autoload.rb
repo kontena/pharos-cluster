632c069fb2d66983a2ff899cb9168867b4dbfbf5
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-09-28 16:59:35 +0300

    String casings and nested hash key transformations

--- lib/pharos/autoload.rb
+++ lib/pharos/autoload.rb
@@ -31,6 +31,8 @@ module Pharos
 
   module CoreExt
     autoload :IPAddrLoopback, 'pharos/core-ext/ip_addr_loopback'
+    autoload :DeepTransformKeys, 'pharos/core-ext/deep_transform_keys'
+    autoload :StringCasing, 'pharos/core-ext/string_casing'
   end
 
   module SSH
