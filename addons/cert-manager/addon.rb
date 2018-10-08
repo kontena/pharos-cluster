6e9cccf3f9144caae15288c6e717d0f54902a50d
Jussi Nummelin <jussi@kontena.io>
2018-10-08 15:35:00 +0300

    update cert-manager to 0.5.0

--- addons/cert-manager/addon.rb
+++ addons/cert-manager/addon.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Pharos.addon 'cert-manager' do
-  version '0.2.5'
+  version '0.5.0'
   license 'Apache License 2.0'
 
   issuer = custom_type {
@@ -20,5 +20,42 @@ Pharos.addon 'cert-manager' do
       required(:email).filled(:str?)
       optional(:server).filled(:str?)
     }
+
+    # Register custom error for LE Acme v1 endpoint validation
+    configure do
+      def self.messages
+        super.merge(en: { errors: { le_acme_v1: "Acme v1 is not supported by CertManager as of version 0.3.0, please change to Acme v2 endpoint!" }})
+      end
+    end
+
+    validate(le_acme_v1: :issuer) do |issuer|
+      if issuer[:name] == 'letsencrypt' && issuer[:server].include?('acme-v01.api.letsencrypt.org')
+        false
+      else
+        true
+      end
+    end
   }
+
+  install {
+
+    apply_resources
+
+    migrate_acme_v2
+  }
+
+  def migrate_acme_v2
+    kube_client.api('certmanager.k8s.io/v1alpha1').resource('issuers', namespace: nil).list.each do |issuer|
+      next unless issuer.spec.acme.server == 'https://acme-v01.api.letsencrypt.org/directory'
+
+      spec = {
+        acme: {
+          server: 'https://acme-v02.api.letsencrypt.org/directory'
+        }
+      }
+      rc = kube_client.client_for_resource(issuer, namespace: issuer.metadata.namespace)
+      rc.merge_patch(issuer.metadata.name, {spec: spec}, namespace: issuer.metadata.namespace, strategic_merge: false)
+    end
+  end
+
 end
