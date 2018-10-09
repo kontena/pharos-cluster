1e8597efa3db6dce00ca574c7f1cf8ede4e14e66
Jussi Nummelin <jussi@kontena.io>
2018-10-09 11:59:52 +0300

    fix default backend image naming & scaling

--- addons/ingress-nginx/addon.rb
+++ addons/ingress-nginx/addon.rb
@@ -27,25 +27,25 @@ Pharos.addon 'ingress-nginx' do
     )
   }
 
-  DEFAULT_BACKEND_ARM64_IMAGE = 'docker.io/kontena/pharos-default-backend-arm64:0.0.2'
-  DEFAULT_BACKEND_IMAGE = 'docker.io/kontena/pharos-default-backend:0.0.2'
-
   def image_name
     return config.default_backend[:image] if config.default_backend&.dig(:image)
 
-    if cpu_arch.name == 'arm64'
-      DEFAULT_BACKEND_ARM64_IMAGE
-    else
-      DEFAULT_BACKEND_IMAGE
-    end
+    "docker.io/kontena/pharos-default-backend-#{cpu_arch.name}:0.0.3"
   end
 
   # ~One replica per 10 workers, min 2
   # @return [Integer]
   def default_backend_replicas
-    r = (@cluster_config.worker_hosts.size / 10.to_f).ceil
+    r = (worker_node_count / 10.to_f).ceil
 
-    return 2 if r < 2
+    return 1 if worker_node_count <= 1 # covers also single node cluster case where master is un-tainted
+    return 2 if r < 2 && worker_node_count > 1
+
     r
+  end
+
+  # Counts the worker nodes
+  def worker_node_count
+    @worker_node_count ||= kube_client.api('v1').resource('nodes').list(labelSelector: 'node-role.kubernetes.io/master!=').size
   end
 end
