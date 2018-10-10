dd0e72d950346b2e262da79f3bc6c99370e146de
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-10-10 12:31:17 +0300

    fix

--- lib/pharos/phases/configure_psp.rb
+++ lib/pharos/phases/configure_psp.rb
@@ -9,7 +9,7 @@ module Pharos
         logger.info { "Configuring default pod security policies ..." }
         apply_stack(
           'psp',
-          default_psp: config.pod_security_policy.default
+          default_psp: @config.pod_security_policy.default_policy
         )
       end
     end
