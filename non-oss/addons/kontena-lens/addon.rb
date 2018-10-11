7b7564856d44e6ad2b4805f3f850654631a6dcfb
Lauri Nevala <lauri.nevala@gmail.com>
2018-10-11 14:29:56 +0300

    Improve roles and ingress rule

--- non-oss/addons/kontena-lens/addon.rb
+++ non-oss/addons/kontena-lens/addon.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
 Pharos.addon 'kontena-lens' do
-  version '0.1.0-dev'
+  version '1.0.0-beta.1'
   license 'Kontena License'
 
   config_schema {
-    optional(:name).filled(:str?)
     optional(:host).filled(:str?)
     optional(:email).filled(:str?)
+  }
+
+  def worker_node_ip
+    worker_node = cluster_config.worker_hosts.first
+    worker_node.address if worker_node
+  end
+
+  install {
+    host = config.host || "lens.#{worker_node_ip}.nip.io"
+    apply_resources(
+      host: host
+    )
   }
 end
