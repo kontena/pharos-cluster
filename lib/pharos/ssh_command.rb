e42928d8e9424a354986f06998ace3071629fcc4
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-10-16 10:11:38 +0300

    Rubocop + refactor

--- lib/pharos/ssh_command.rb
+++ lib/pharos/ssh_command.rb
@@ -34,39 +34,49 @@ module Pharos
     end
 
     def execute
-      if command_list.empty?
-        exit_statuses = hosts.map do |host|
-          target = "#{host.user}@#{use_private? ? host.private_address : host.address}"
-          puts pastel.green("==> Opening a session to #{target} ..") unless !$stdout.tty?
-          system('ssh', '-i', host.ssh_key_path, target)
-        end
-        exit(1) unless exit_statuses.all?(&:itself)
-      elsif hosts.size == 1
-        result = ssh_manager.client_for(hosts.first).exec(command_list)
-        puts result.output
-        exit result.exit_status
-      else
-        threads = hosts.map do |host|
-          Thread.new do
-            begin
-              [host, ssh_manager.client_for(host).exec(command_list)]
-            rescue => ex
-              [
-                host,
-                Pharos::SSH::RemoteCommand::Result.new.tap do |r|
-                  r.output << "#{ex.message}"
-                end
-              ]
-            end
+      exit run_interactive if command_list.empty?
+      exit run_single(hosts.first) if hosts.size == 1
+      exit run_parallel
+    end
+
+    private
+
+    def run_interactive
+      exit_statuses = hosts.map do |host|
+        target = "#{host.user}@#{use_private? ? host.private_address : host.address}"
+        puts pastel.green("==> Opening a session to #{target} ..") unless !$stdout.tty?
+        system('ssh', '-i', host.ssh_key_path, target)
+      end
+      exit_statuses.all?(&:itself) ? 0 : 1
+    end
+
+    def run_single(host)
+      result = ssh_manager.client_for(host).exec(command_list)
+      puts result.output
+      result.exit_status
+    end
+
+    def run_parallel
+      threads = hosts.map do |host|
+        Thread.new do
+          begin
+            [host, ssh_manager.client_for(host).exec(command_list)]
+          rescue StandardError => ex
+            [
+              host,
+              Pharos::SSH::RemoteCommand::Result.new.tap do |r|
+                r.output << ex.message
+              end
+            ]
           end
         end
-        results = threads.map(&:value)
-        results.each do |host, result|
-          puts pastel.send(result.exit_status.zero? ? :green : :red, "==> Result from #{host.address}")
-          puts result.output.gsub(/^/, "  ")
-        end
-        exit(1) if results.none? { |_, result| result.success? }
       end
+      results = threads.map(&:value)
+      results.each do |host, result|
+        puts pastel.send(result.exit_status.zero? ? :green : :red, "==> Result from #{host.address}")
+        puts result.output.gsub(/^/, "  ")
+      end
+      results.all? { |_, result| result.success? } ? 0 : 1
     end
 
     def ssh_manager
