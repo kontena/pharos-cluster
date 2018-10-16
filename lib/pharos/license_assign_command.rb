170a1dc53822f2239d8593c00296bf8d2d524403
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-10-16 14:59:30 +0300

    Slightly more elegant updating

--- lib/pharos/license_assign_command.rb
+++ lib/pharos/license_assign_command.rb
@@ -23,15 +23,15 @@ module Pharos
 
     def execute
       signal_usage_error 'invalid LICENSE_KEY format' unless license_key.match?(/^\h{8}-(?:\h{4}-){3}\h{12}$/)
-      retry_times = 0
+
+      logger.info "Deleted existing subscription token from pharos cluster secrets" if try_delete_existing
+
       ssh.exec!("kubectl create secret generic pharos-cluster --namespace=kube-system --from-literal=key=#{subscription_token.shellescape}")
       logger.info "Added subscription token to pharos cluster secrets"
-    rescue Pharos::SSH::RemoteCommand::ExecError
-      retry_times += 1
-      raise if retry_times > 1
-      ssh.exec!("kubectl delete secret pharos-cluster --namespace=kube-system")
-      logger.info "Deleted existing subscription token from pharos cluster secrets"
-      retry
+    end
+
+    def try_delete_existing
+      ssh.exec("kubectl delete secret pharos-cluster --namespace=kube-system").success?
     end
 
     def ssh
