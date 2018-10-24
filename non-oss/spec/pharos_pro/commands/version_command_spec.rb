14d31284af09ae67e3ec7a3ee846152eb2002b21
Kimmo Lehto <kimmo.lehto@gmail.com>
2018-10-24 14:30:11 +0300

    Restore version back to 2.0.0-alpha.2 (#711)

--- non-oss/spec/pharos_pro/commands/version_command_spec.rb
+++ non-oss/spec/pharos_pro/commands/version_command_spec.rb
@@ -1,13 +1,17 @@
 describe Pharos::VersionCommand do
   subject { described_class.new('') }
 
+  it 'outputs pharos version' do
+    expect{subject.run([])}.to output(/Kontena Pharos:\n[^\n]+?(\d+\.\d+\.\d+)/).to_stdout
+  end
+
   it 'outputs version without +oss' do
-    expect{subject.run([])}.to output(/Kontena Pharos:\n.+?version \d+\.\d+\.\d+(?:\-[\+]+)?\n/m).to_stdout
+    expect{subject.run([])}.not_to output(/\+oss$/m).to_stdout
   end
 
   context '--version' do
     it 'outputs version with +oss' do
-      expect{subject.run(['--version'])}.to output(/.+?version \d+\.\d+\.\d+(?:\-[\+]+)?\n/).to_stdout
+      expect{subject.run(['--version'])}.not_to output(/\+oss$/m).to_stdout
     end
   end
 end
