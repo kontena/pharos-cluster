95a19207c2b701f902e5d9f7c23a789ee45517a2
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-11-01 15:04:07 +0200

    cleanup

--- lib/pharos/phases/configure_host.rb
+++ lib/pharos/phases/configure_host.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'singleton'
-
 module Pharos
   module Phases
     class ConfigureHost < Pharos::Phase
--- lib/pharos/ssh/manager.rb
+++ lib/pharos/ssh/manager.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'singleton'
 require 'net/ssh'
 require 'net/ssh/proxy/jump'
 
