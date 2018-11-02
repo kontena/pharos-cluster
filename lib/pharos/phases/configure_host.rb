c15e0583096601b397df0ae5677c60b9af993fe9
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-11-02 10:45:52 +0200

    cleanup / refactor

--- lib/pharos/phases/configure_host.rb
+++ lib/pharos/phases/configure_host.rb
@@ -23,26 +23,12 @@ module Pharos
         logger.info { "Configuring netfilter ..." }
         host_configurer.configure_netfilter
 
-        if @host.new? || host_configurer.configure_container_runtime_safe?
-          logger.info { "Configuring container runtime (#{@host.container_runtime}) packages ..." }
-          host_configurer.configure_container_runtime
-        else
-          logger.info { "Reconfiguration of container runtime (#{@host.container_runtime}) might affect workloads, switching to a safe mode ..." }
-          mutex.synchronize do
-            if master_healthy?
-              logger.info { "Draining node ..." }
-              master_ssh.exec!("kubectl drain --force --ignore-daemonsets --delete-local-data #{@host.hostname}")
-            end
-            logger.info { "Reconfiguring container runtime (#{@host.container_runtime}) packages ..." }
-            host_configurer.configure_container_runtime
-            if master_healthy?
-              logger.info { "Uncordoning node ..." }
-              master_ssh.exec!("kubectl uncordon #{@host.hostname}")
-              logger.info { "Waiting for node to be ready ..." }
-              sleep 10 until master_ssh.exec("kubectl get nodes -o jsonpath=\"{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}\" | grep 'Ready=True'").success?
-            end
-          end
-        end
+        configure_container_runtime
+      end
+
+      def configure_container_runtime
+        logger.info { "Configuring container runtime (#{@host.container_runtime}) packages ..." }
+        host_configurer.configure_container_runtime
       end
 
       def master_ssh
