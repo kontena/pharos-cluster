f1e840564b355f2f89028e0b4f464faaf7f33dc2
Kimmo Lehto <kimmo.lehto@gmail.com>
2018-11-22 11:20:55 +0200

    Bundle an up-to-date cacert.pem file into binary during build (#827)

--- build/drone/ubuntu.sh
+++ build/drone/ubuntu.sh
@@ -7,6 +7,10 @@ apt-get update -y
 apt-get install -y -q squashfs-tools build-essential ruby bison ruby-dev git-core texinfo curl
 curl -sL https://dl.bintray.com/kontena/ruby-packer/0.5.0-dev/rubyc-linux-amd64.gz | gunzip > /usr/local/bin/rubyc
 chmod +x /usr/local/bin/rubyc
+
+# Download updated SSL certs
+curl -sL https://curl.haxx.se/ca/cacert.pem > data/cacert.pem
+
 gem install bundler
 version=${DRONE_TAG#"v"}
 package="pharos-cluster-linux-amd64-${version}"
