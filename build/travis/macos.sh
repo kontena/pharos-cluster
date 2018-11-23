f8fe3a187d828ebb3fa51aa57358c1c504d82091
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-11-23 14:20:30 +0200

    Remove the bundled SSL cert that did not get used

--- build/travis/macos.sh
+++ build/travis/macos.sh
@@ -8,10 +8,6 @@ brew install squashfs
 curl -sL https://dl.bintray.com/kontena/ruby-packer/0.5.0-dev/rubyc-darwin-amd64.gz | gunzip > /usr/local/bin/rubyc
 chmod +x /usr/local/bin/rubyc
 
-# Download updated SSL certs
-mkdir -p data
-curl -sL https://curl.haxx.se/ca/cacert.pem > data/cacert.pem
-
 version=${TRAVIS_TAG#"v"}
 package="pharos-cluster-darwin-amd64-${version}"
 rubyc -o "$package" --make-args=--silent pharos-cluster
--- build/travis/macos_oss.sh
+++ build/travis/macos_oss.sh
@@ -9,10 +9,6 @@ curl -sL https://dl.bintray.com/kontena/ruby-packer/0.5.0-dev/rubyc-darwin-amd64
 curl -sL https://curl.haxx.se/ca/cacert.pem > data/cacert.pem
 chmod +x /usr/local/bin/rubyc
 
-# Download updated SSL certs
-mkdir -p data
-curl -sL https://curl.haxx.se/ca/cacert.pem > data/cacert.pem
-
 version=${TRAVIS_TAG#"v"}
 package="pharos-cluster-darwin-amd64-${version}+oss"
 rubyc -o "$package" --make-args=--silent pharos-cluster
