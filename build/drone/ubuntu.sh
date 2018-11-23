94ba665baffdfb370cb19a285b264c190af7e87c
Kimmo Lehto <kimmo.lehto@gmail.com>
2018-11-23 15:46:53 +0200

    Remove the bundled SSL cert that did not get used (#837)

--- build/drone/ubuntu.sh
+++ build/drone/ubuntu.sh
@@ -8,10 +8,6 @@ apt-get install -y -q squashfs-tools build-essential ruby bison ruby-dev git-cor
 curl -sL https://dl.bintray.com/kontena/ruby-packer/0.5.0-dev/rubyc-linux-amd64.gz | gunzip > /usr/local/bin/rubyc
 chmod +x /usr/local/bin/rubyc
 
-# Download updated SSL certs
-mkdir -p data
-curl -sL https://curl.haxx.se/ca/cacert.pem > data/cacert.pem
-
 gem install bundler
 version=${DRONE_TAG#"v"}
 package="pharos-cluster-linux-amd64-${version}"
--- build/travis/macos_oss.sh
+++ build/travis/macos_oss.sh
@@ -6,12 +6,7 @@ rm -rf non-oss/
 
 brew install squashfs
 curl -sL https://dl.bintray.com/kontena/ruby-packer/0.5.0-dev/rubyc-darwin-amd64.gz | gunzip > /usr/local/bin/rubyc
-curl -sL https://curl.haxx.se/ca/cacert.pem > data/cacert.pem
 chmod +x /usr/local/bin/rubyc
-
-# Download updated SSL certs
-mkdir -p data
-curl -sL https://curl.haxx.se/ca/cacert.pem > data/cacert.pem
 
 version=${TRAVIS_TAG#"v"}
 package="pharos-cluster-darwin-amd64-${version}+oss"
