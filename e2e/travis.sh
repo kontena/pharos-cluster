2acb01cc95ff878c247adcf5a1395859eeec589a
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-11-24 10:35:44 +0200

    Travis e2e (#840)

---
+++ e2e/travis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ue
+
+ssh-keygen -t rsa -f ~/.ssh/id_rsa_travis -N ""
+cat ~/.ssh/id_rsa_travis.pub > ~/.ssh/authorized_keys
+chmod 0600 ~/.ssh/authorized_keys
+
+ifconfig
+
+envsubst < e2e/cluster.yml > cluster.yml
+
+bundle exec bin/pharos-cluster
+bundle exec bin/pharos-cluster -v
+bundle exec bin/pharos-cluster version
+bundle exec bin/pharos-cluster up -d -y -c cluster.yml
+bundle exec bin/pharos-cluster ssh --role master -c cluster.yml -- kubectl get nodes
+# TODO: re-up dies with exit code 141
+#bundle exec bin/pharos-cluster up -d -y -c cluster.yml
+bundle exec bin/pharos-cluster reset -d -y -c cluster.yml
