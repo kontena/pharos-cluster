c7751fd71a680a8a3e6b54ce00f4a27ada2f3f97
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-11-24 10:10:17 +0200

    test also calico

--- e2e/travis.sh
+++ e2e/travis.sh
@@ -8,11 +8,13 @@ chmod 0600 ~/.ssh/authorized_keys
 
 ifconfig
 
+envsubst < e2e/cluster.yml > cluster.yml
+
 bundle exec bin/pharos-cluster
 bundle exec bin/pharos-cluster -v
 bundle exec bin/pharos-cluster version
-bundle exec bin/pharos-cluster up -d -y -c e2e/cluster.yml
-bundle exec bin/pharos-cluster ssh --role master -c e2e/cluster.yml -- kubectl get nodes
+bundle exec bin/pharos-cluster up -d -y -c cluster.yml
+bundle exec bin/pharos-cluster ssh --role master -c cluster.yml -- kubectl get nodes
 # TODO: re-up dies with exit code 141
-#bundle exec bin/pharos-cluster up -d -y -c e2e/cluster.yml
-bundle exec bin/pharos-cluster reset -d -y -c e2e/cluster.yml
+#bundle exec bin/pharos-cluster up -d -y -c cluster.yml
+bundle exec bin/pharos-cluster reset -d -y -c cluster.yml
