719cea14fbd31adc19bf7738cdf7b71d0530bf9a
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-12-04 15:19:03 +0200

    smoke

---
+++ e2e/drone.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ue
+
+bundle exec bin/pharos-cluster
+bundle exec bin/pharos-cluster -v
+bundle exec bin/pharos-cluster version
+bundle exec bin/pharos-cluster up -d -y -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json
+bundle exec bin/pharos-cluster ssh --role master -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json -- kubectl get nodes -o wide
+bundle exec bin/pharos-cluster up -d -y -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json
+bundle exec bin/pharos-cluster reset -d -y -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json
