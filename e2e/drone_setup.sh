ba7aaf78c4bef107e049711d6b3b9ca3ad69be6d
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-12-04 15:21:58 +0200

    fix

--- e2e/drone_setup.sh
+++ e2e/drone_setup.sh
@@ -4,4 +4,4 @@ set -e
 
 cd e2e/digitalocean
 terraform init
-terraform apply -auto-approve -var "cluster_name=${DRONE_BUILD_NUMBER}-do" && terraform output -json tf.json
+terraform apply -auto-approve -var "cluster_name=${DRONE_BUILD_NUMBER}-do" && terraform output -json > tf.json
