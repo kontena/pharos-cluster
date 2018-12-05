bf69ac4336cbf450a3824a0b66cd81332d579d7f
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2018-12-05 12:39:53 +0200

    build gem and use it

--- e2e/drone.sh
+++ e2e/drone.sh
@@ -4,12 +4,12 @@ set -ue
 
 export PHAROS_NON_OSS=true
 
-PHAROS="bundle exec bin/pharos-cluster"
-
 bundle install
-$PHAROS
-$PHAROS -v
-$PHAROS version
-$PHAROS up -y -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json
-$PHAROS ssh --role master -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json -- kubectl get nodes -o wide
-$PHAROS up -y -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json
+gem build pharos-cluster.gemspec
+gem install pharos-cluster*.gem
+pharos-cluster
+pharos-cluster -v
+pharos-cluster version
+pharos-cluster up -y -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json
+pharos-cluster ssh --role master -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json -- kubectl get nodes -o wide
+pharos-cluster up -y -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json
