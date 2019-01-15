719806cc96a65b378258057d6be507c3488813d0
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-01-15 15:14:43 +0200

    update rubyc to 2.6.0-0.6.0

--- build/drone/ubuntu.sh
+++ build/drone/ubuntu.sh
@@ -8,7 +8,7 @@ apt-get install -y -q squashfs-tools build-essential ruby bison ruby-dev git-cor
 
 update-ca-certificates
 
-curl -sL https://dl.bintray.com/kontena/ruby-packer/0.5.0-dev/rubyc-0.5.0-extra2-linux-amd64.gz | gunzip > /usr/local/bin/rubyc
+curl -sL https://github.com/kontena/ruby-packer/releases/download/2.6.0-0.6.0/rubyc-2.6.0-0.6.0-linux-amd64.gz | gunzip > /usr/local/bin/rubyc
 chmod +x /usr/local/bin/rubyc
 
 version=${DRONE_TAG#"v"}
