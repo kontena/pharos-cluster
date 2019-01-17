5e6d06f4a2d59ad1a5a55c5c1792b87a91217404
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-01-17 13:52:34 +0200

    addon configs & etcd-scraper-proxy

--- non-oss/pharos_pro/addons/kontena-stats/addon.rb
+++ non-oss/pharos_pro/addons/kontena-stats/addon.rb
@@ -1,5 +1,19 @@
+# frozen_string_literal: true
+
 Pharos.addon('kontena-stats') do
   prometheus_version '2.6.1'
   version "#{prometheus_version}+kontena.1"
   license 'Kontena License'
+
+  config {
+    attribute :replicas, Pharos::Types::Integer.default(1)
+    attribute :tolerations, Pharos::Types::Array.default([])
+    attribute :node_selector, Pharos::Types::Hash.default({})
+  }
+
+  config_schema {
+    optional(:replicas).filled(:int?)
+    optional(:tolerations).each(:hash?)
+    optional(:node_selector).filled(:hash?)
+  }
 end
