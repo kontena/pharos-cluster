fa75f69f0fd3489d0ee58afeb1ec4894f900b277
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-01-21 15:06:14 +0200

    refactor configuration

--- lib/pharos/phases/configure_firewalld.rb
+++ lib/pharos/phases/configure_firewalld.rb
@@ -7,11 +7,21 @@ module Pharos
 
       def call
         logger.info { 'Configuring firewalld rules ...' }
+
+        write_config('services/pharos-master.xml', pharos_master_service) if @host.master?
+        write_config('services/pharos-worker.xml', pharos_worker_service)
+        write_config('ipsets/pharos.xml', pharos_ipset)
+
         exec_script(
           'configure-firewalld.sh',
-          ROLE: @host.role,
-          PEER_ADDRESSES: trusted_addresses.join("\n")
+          ROLE: @host.role
         )
+      end
+
+      # @param file [String]
+      # @param contents [String]
+      def write_config(file, contents)
+        @host.ssh.file(File.join('/etc/firewalld/', file)).write(contents)
       end
 
       # @return [Array<String>]
@@ -19,6 +29,41 @@ module Pharos
         @config.hosts.flat_map { |host|
           [host.address, host.private_address, host.private_interface_address].compact.uniq
         }
+      end
+
+      # @return [String]
+      def pharos_master_service
+        parse_resource_file(
+          'firewalld/service.xml.erb',
+          name: 'pharos-master',
+          description: 'Kontena Pharos master host service',
+          ports: [
+            { port: 6443, protocol: 'tcp' },
+            { port: 22, protocol: 'tcp' }
+          ]
+        )
+      end
+
+      # @return [String]
+      def pharos_worker_service
+        parse_resource_file(
+          'firewalld/service.xml.erb',
+          name: 'pharos-worker',
+          description: 'Kontena Pharos worker host service',
+          ports: [
+            { port: 80, protocol: 'tcp' },
+            { port: 443, protocol: 'tcp' }
+          ]
+        )
+      end
+
+      # @return [String]
+      def pharos_ipset
+        parse_resource_file(
+          'firewalld/ipset.xml.erb',
+          name: 'pharos',
+          entries: trusted_addresses
+        )
       end
     end
   end
--- lib/pharos/scripts/configure-firewalld.sh
+++ lib/pharos/scripts/configure-firewalld.sh
@@ -2,38 +2,15 @@
 
 set -e
 
-add_port() {
-    local port=$1
-    local zone=$2
-    if ! firewall-cmd --query-port "$port" --zone "$zone" ; then
-        firewall-cmd --permanent --add-port "$port" --zone "$zone"
-    fi
-}
-
-add_source() {
-    local source=$1
-    local zone=$2
-    if ! firewall-cmd --query-source "$source" --zone "$zone" ; then
-        firewall-cmd --permanent --add-source "$source" --zone "$zone"
-    fi
-}
-
 if ! systemctl is-active --quiet firewalld; then
     systemctl enable firewalld
     systemctl start firewalld
 fi
 
-add_port "22/tcp" "public"
-add_port "80/tcp" "public"
-add_port "443/tcp" "public"
-add_port "30000-32767/tcp" "public"
-
+firewall-cmd --reload
 if [ "$ROLE" = "master" ]; then
-    add_port "6334/tcp" "public"
+    firewall-cmd --permanent --add-service pharos-master
 fi
-
-for peer in $PEER_ADDRESSES ; do
-    add_source "$peer" "trusted"
-done
-
+firewall-cmd --permanent --add-service pharos-worker
+firewall-cmd --permanent --add-source ipset:pharos --zone trusted
 firewall-cmd --reload
