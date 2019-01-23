17da21cc08ed254f3ad32e084da53c36f265c02f
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-01-23 09:28:13 +0200

    rubocop

--- lib/pharos/phases/pull_master_images.rb
+++ lib/pharos/phases/pull_master_images.rb
@@ -6,7 +6,7 @@ module Pharos
       title "Pull control plane images"
 
       def call
-        logger.info { "Pulling control plane images ..."}
+        logger.info { "Pulling control plane images ..." }
         cfg = kubeadm.generate_yaml_config
         ssh.tempfile(content: cfg, prefix: "kubeadm.cfg") do |tmp_file|
           @host.ssh.exec!("sudo kubeadm config images pull --config #{tmp_file}")
