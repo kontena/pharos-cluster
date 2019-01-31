02d3f69cd747bf3bc9441199cc5933b36e8a5f00
Kimmo Lehto <kimmo.lehto@kontena.io>
2019-01-31 08:23:19 +0200

    Remove autoload

--- lib/pharos/autoload.rb
+++ lib/pharos/autoload.rb
@@ -47,7 +47,6 @@ module Pharos
   module CoreExt
     autoload :IPAddrLoopback, 'pharos/core-ext/ip_addr_loopback'
     autoload :DeepTransformKeys, 'pharos/core-ext/deep_transform_keys'
-    autoload :DeepKeys, 'pharos/core-ext/deep_keys'
     autoload :StringCasing, 'pharos/core-ext/string_casing'
   end
 
