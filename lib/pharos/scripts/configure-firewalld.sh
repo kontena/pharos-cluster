e550544f071ab4328dedc2bea36a06cd38c1e8cd
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-02-01 21:41:28 +0200

    firewalld: masquerade default zone

--- lib/pharos/scripts/configure-firewalld.sh
+++ lib/pharos/scripts/configure-firewalld.sh
@@ -13,5 +13,5 @@ if [ "$ROLE" = "master" ]; then
 fi
 firewall-cmd --permanent --add-service pharos-worker
 firewall-cmd --permanent --add-source ipset:pharos --zone trusted
-firewall-cmd --add-masquerade --permanent --zone trusted
+firewall-cmd --add-masquerade --permanent
 firewall-cmd --reload
