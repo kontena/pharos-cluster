9d297dce68253b5cc0615e68bbd9bafc48feb060
Kimmo Lehto <kimmo.lehto@kontena.io>
2019-02-11 13:50:15 +0200

    Remove unnecessary method

--- lib/pharos/phase.rb
+++ lib/pharos/phase.rb
@@ -81,11 +81,6 @@ module Pharos
       @config.master_host
     end
 
-    # @return [Pharos::Transport::SSH,Pharos::Transport::Local]
-    def master_transport
-      return master_host.transport
-    end
-
     # @return [K8s::Client]
     def kube_client
       fail "Phase #{self.class.name} does not have kubeconfig cluster_context" unless cluster_context['kubeconfig']
