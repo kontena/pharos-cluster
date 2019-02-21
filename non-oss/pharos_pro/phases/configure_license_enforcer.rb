9f8b6c0fcf3a21ba4192127f08e59b7e05c32fe7
Jussi Nummelin <jussi@kontena.io>
2019-02-21 21:18:29 +0200

    use multiarch repo for image

--- non-oss/pharos_pro/phases/configure_license_enforcer.rb
+++ non-oss/pharos_pro/phases/configure_license_enforcer.rb
@@ -5,13 +5,21 @@ module Pharos
     class ConfigureLicenseEnforcer < Pharos::Phase
       title "Configure license enforcement"
 
+      LICENSE_ENFORCER_VERSION = '0.1.0'
       RESOURCE_PATH = Pathname.new(File.expand_path(File.join(__dir__, '..', 'resources', 'license-enforcer'))).freeze
+
+      register_component(
+        name: 'PHAROS-LICENSE-ENFORCER', version: LICENSE_ENFORCER_VERSION, license: 'Kontena License'
+      )
 
       def call
         logger.info { "Configuring icense enforcement on Pharos PRO ..." }
-        logger.info { "Resource path: #{RESOURCE_PATH}" }
 
-        stack = Pharos::Kube.stack('license-enforcer', RESOURCE_PATH)
+        stack = Pharos::Kube.stack('license-enforcer',
+          RESOURCE_PATH,
+          version: LICENSE_ENFORCER_VERSION,
+          image_repository: @config.image_repository
+        )
 
         stack.apply(kube_client)
       end
