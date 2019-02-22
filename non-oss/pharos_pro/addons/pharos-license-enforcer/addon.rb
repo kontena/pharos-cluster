c20ae4b6392751d39890c741863d786d7fa3b464
Kimmo Lehto <kimmo.lehto@kontena.io>
2019-02-22 12:39:36 +0200

    Add post install message

--- non-oss/pharos_pro/addons/pharos-license-enforcer/addon.rb
+++ non-oss/pharos_pro/addons/pharos-license-enforcer/addon.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
-Pharos.addon 'kontena-license-enforcer' do
+Pharos.addon 'pharos-license-enforcer' do
   version '0.1.0'
   license 'Kontena License'
 
   enable!
+
+  install do
+    apply_resources
+    post_install_message(<<~POST_INSTALL_MESSAGE)
+      To assign a license to the cluster, use the #{"pharos license assign".cyan} command.
+    POST_INSTALL_MESSAGE
+  end
 end
