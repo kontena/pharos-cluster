0f3f9b682dc9a13776bd5f89604b47f87d9c7760
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-02-22 13:20:35 +0200

    Add ipalloc_default_subnet config option to Weave (#1100)

--- lib/pharos/phases/configure_weave.rb
+++ lib/pharos/phases/configure_weave.rb
@@ -73,6 +73,7 @@ module Pharos
       # @return [Array<String>]
       def extra_args
         args = []
+        args << "--ipalloc-default-subnet=#{@config.network&.weave&.ipalloc_default_subnet}" if @config.network&.weave&.ipalloc_default_subnet
         args << "--trusted-subnets=#{trusted_subnets.join(',')}" unless trusted_subnets.empty?
         args << "--no-discovery" if flying_shuttle?
 
