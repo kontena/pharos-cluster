4e2dda4794f8656fca1016a8b8d79465f8806959
Kimmo Lehto <kimmo.lehto@gmail.com>
2019-02-22 11:13:40 +0200

    Fix DeepTransformKeys and spec (#1091)

--- spec/pharos/core_ext/deep_transform_keys_spec.rb
+++ spec/pharos/core_ext/deep_transform_keys_spec.rb
@@ -50,4 +50,27 @@ describe Pharos::CoreExt::DeepTransformKeys do
       end
     end
   end
+
+  describe '#deep_symbolize_keys' do
+    context 'as a module' do
+      subject { { 'foo' => { 'bar' => [ { 'baz' => 1 } ] } } }
+      it 'deep symbolizes keys' do
+        subject.extend(described_class)
+        expect(subject.deep_symbolize_keys).to eq(
+          { foo: { bar: [ { baz: 1 } ] } }
+        )
+      end
+    end
+
+    context 'as a refinement' do
+      using Pharos::CoreExt::DeepTransformKeys
+      subject { { 'foo' => { 'bar' => [ { 'baz' => 1 } ] } } }
+
+      it 'deep symbolizes keys' do
+        expect(subject.deep_symbolize_keys).to eq(
+          { foo: { bar: [ { baz: 1 } ] } }
+        )
+      end
+    end
+  end
 end
