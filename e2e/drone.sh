e041fa353cd5a8aef672a867477942774fb4a042
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-02-28 20:07:58 +0200

    debug e2e

--- e2e/drone.sh
+++ e2e/drone.sh
@@ -10,7 +10,7 @@ gem install pharos-cluster*.gem
 pharos-cluster
 pharos-cluster -v
 pharos-cluster version
-pharos-cluster up -y -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json
+pharos-cluster up -y --debug -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json
 pharos-cluster kubeconfig -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json > kubeconfig.e2e && ls -al kubeconfig.e2e && rm -f kubeconfig.e2e
 pharos-cluster exec --role master -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json -- kubectl get nodes -o wide
 pharos-cluster up -y -c e2e/digitalocean/cluster.yml --tf-json e2e/digitalocean/tf.json
