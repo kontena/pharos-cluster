aa845eb4ec0f6c19ad7e38e99abe36b4530c6d0b
Lauri Nevala <lauri.nevala@gmail.com>
2019-03-07 11:38:12 +0200

    Read cluster name from pharos-config configmap (#1147)
    Read cluster name from pharos-config on license assign

--- non-oss/pharos_pro/commands/license_assign_command.rb
+++ non-oss/pharos_pro/commands/license_assign_command.rb
@@ -2,9 +2,11 @@
 
 module Pharos
   class LicenseAssignCommand < Pharos::Command
-    options :load_config, :license_key
+    using Pharos::CoreExt::Colorize
 
-    parameter "[LICENSE_KEY]", "kontena pharos license key or subscription token"
+    options :load_config, :tf_json
+
+    parameter "[LICENSE_KEY]", "kontena pharos license key (default: <stdin>)"
     option '--description', 'DESCRIPTION', "license description [DEPRECATED]", hidden: true
     option %w(-f --force), :flag, "force assign invalid/expired token"
     option '--token', :flag, "display license subscription token" do
@@ -18,7 +20,7 @@ module Pharos
 
     def execute
       warn '[DEPRECATED] the --description option is deprecated and will be ignored' if description
-
+      cluster_manager
       puts decorate_license
 
       unless jwt_token.valid? || force?
@@ -27,8 +29,103 @@ module Pharos
 
       Dir.chdir(config_yaml.dirname) do
         master_host.transport.connect
-        master_host.transport.exec!("kubectl create secret generic pharos-cluster --namespace=kube-system --from-literal='license.jwt=#{jwt_token.token}' --dry-run -o yaml | kubectl apply -f -")
-        logger.info "Added subscription token to pharos cluster secrets"
+        master_host.transport.exec!("kubectl create secret generic pharos-license --namespace=kube-system --from-literal='license.jwt=#{jwt_token.token}' --dry-run -o yaml | kubectl apply -f -")
+        logger.info "Assigned the subscription token successfully to the cluster.".green
+      end
+    end
+
+    private
+
+    def default_license_key
+      if tty?
+        prompt.ask('Enter Kontena Pharos license key:')
+      elsif !stdin_eof?
+        $stdin.read
+      else
+        signal_usage_error 'LICENSE_KEY required'
+      end
+    end
+
+    def jwt_token
+      @jwt_token ||= if license_key.match?(/^\h{8}-(?:\h{4}-){3}\h{12}$/)
+                       Pharos::LicenseKey.new(subscription_token, cluster_id: cluster_id)
+                     else
+                       begin
+                         load_config
+                         c_id = cluster_id
+                       rescue StandardError
+                         c_id = nil
+                       end
+                       Pharos::LicenseKey.new(license_key, cluster_id: c_id)
+                     end
+    end
+
+    def master_host
+      @master_host ||= load_config.master_host
+    end
+
+    def cluster_info
+      @cluster_info ||= cluster_info_configmap
+    rescue K8s::Error::NotFound => ex
+      signal_error "Failed to fetch cluster-info: #{ex.class.name} : #{ex.message}"
+    end
+
+    def cluster_info_configmap
+      kube_client = load_config.kube_client(cluster_manager.context['kubeconfig'])
+      kube_client.api('v1').resource('configmaps', namespace: 'kube-public').get('cluster-info')
+    end
+
+    def cluster_id
+      cluster_info.dig('metadata', 'uid')
+    end
+
+    def cluster_name
+      load_config.name
+    rescue StandardError => ex
+      signal_error "Failed to parse cluster name from pharos-config.pharos-cluster-name: #{ex.class.name} : #{ex.message}"
+    end
+
+    def subscription_token_request
+      logger.info "Exchanging license key for a subscription token" if $stdout.tty?
+
+      Excon.post(
+        'https://get.pharos.sh/api/licenses/%<key>s/assign' % { key: license_key },
+        body: JSON.dump(
+          data: {
+            attributes: {
+              'cluster-id' => cluster_id,
+              'description' => cluster_name
+            }
+          }
+        ),
+        headers: {
+          'Accept' => 'application/json',
+          'Content-Type' => 'application/json',
+          'User-Agent' => "pharos-cluster/#{Pharos.version}"
+        }
+      )
+    end
+
+    def subscription_token
+      response = JSON.parse(subscription_token_request.body)
+      signal_error response['errors'].map { |error| error['title'] }.join(', ') if response['errors']
+      response.dig('data', 'attributes', 'license-token', 'jwt') || signal_error('invalid response')
+    end
+
+    def decorate_license
+      lexer = Rouge::Lexers::YAML.new
+      if color?
+        rouge.format(lexer.lex(jwt_token.to_h.to_yaml.delete_prefix("---\n")))
+      else
+        jwt_token.to_h.to_yaml.delete_prefix("---\n")
+      end
+    end
+
+    def cluster_manager
+      @cluster_manager ||= ClusterManager.new(load_config.tap { |c| c.hosts.keep_if(&:master?) }).tap do |cluster_manager|
+        puts "==> Sharpening tools ...".green
+        cluster_manager.load
+        cluster_manager.gather_facts
       end
     end
   end
--- non-oss/spec/pharos_pro/commands/license_assign_command_spec.rb
+++ non-oss/spec/pharos_pro/commands/license_assign_command_spec.rb
@@ -8,28 +8,26 @@ describe Pharos::LicenseAssignCommand do
   let(:config) { Pharos::Config.new(hosts: [host]) }
   let(:ssh) { instance_double(Pharos::Transport::SSH) }
   let(:http_client) { spy }
+  let(:cluster_manager) { instance_double(Pharos::ClusterManager) }
   let(:license_token) { 'abcd' }
   let(:success_response) { JSON.dump(data: { attributes: { 'license-token': { token: license_token, jwt: '123' } } }) }
   let(:token) { instance_double(Pharos::LicenseKey, valid?: true, token: '123') }
+  let(:cluster_info) { K8s::Resource.new({ metadata: { uid: '6c6289c0-1fb0-11e9-bac4-02f41f34da68' }})}
 
   before do
     allow(subject).to receive(:decorate_license).and_return('')
     allow(subject).to receive(:config_yaml).and_return(double(dirname: __dir__))
     allow(subject).to receive(:load_config).and_return(config)
     allow(host).to receive(:transport).and_return(ssh)
+    allow(subject).to receive(:cluster_info).and_return(cluster_info)
     allow(Pharos::LicenseKey).to receive(:new).with('123', cluster_id: '6c6289c0-1fb0-11e9-bac4-02f41f34da68').and_return(token)
     stub_const("Excon", http_client)
     allow(ssh).to receive(:connect)
-    allow(ssh).to receive(:exec!).with('kubectl get configmap --namespace kube-public -o yaml cluster-info').and_return(<<~EOS)
-      data:
-        kubeconfig: |
-          clusters:
-          - cluster:
-              server: https://localhost:6443
-            name: "foo"
-      metadata:
-        uid: 6c6289c0-1fb0-11e9-bac4-02f41f34da68
-    EOS
+
+
+    allow(cluster_manager).to receive(:load).and_return true
+    allow(cluster_manager).to receive(:gather_facts).and_return true
+    allow(subject).to receive(:cluster_manager).and_return(cluster_manager)
   end
 
   describe '#execute' do
@@ -37,8 +35,14 @@ describe Pharos::LicenseAssignCommand do
       allow(http_client).to receive(:post).and_return(double(body: success_response))
     end
 
+    it 'signals error if cluster-info not found' do
+      allow(subject).to receive(:cluster_info).and_raise(K8s::Error::NotFound.new('GET', 'path', 404, 'Not found'))
+      expect(subject).to receive(:signal_error)
+      subject.run([license_key])
+    end
+
     it 'runs kubectl on master' do
-      expect(ssh).to receive(:exec!).with("kubectl create secret generic pharos-cluster --namespace=kube-system --from-literal='license.jwt=123' --dry-run -o yaml | kubectl apply -f -")
+      expect(ssh).to receive(:exec!).with("kubectl create secret generic pharos-license --namespace=kube-system --from-literal='license.jwt=123' --dry-run -o yaml | kubectl apply -f -")
       subject.run([license_key])
     end
   end
