04735c399ac28d5ac4fe8bc7874eafa8a0de9cc2
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-03-13 19:22:16 +0200

    Improve oss upgrade warning (#1177)

--- lib/pharos/up_command.rb
+++ lib/pharos/up_command.rb
@@ -96,10 +96,13 @@ module Pharos
         if context['unsafe_upgrade']
           if force?
             puts
-            puts "WARNING:".red + " using --force to attempt an unsafe upgrade, this can break your cluster."
+            puts "WARNING:".red + " using --force to attempt an unsafe upgrade, this might cause downtime."
           else
-            signal_error "Unsupported upgrade path. You may try to force the upgrade by running\n" \
-                         "the command with --force or use the Kontena Pharos Pro version."
+            error_message = <<~ERROR_MSG
+              Upgrading to version #{Pharos.version} might cause downtime. You may force the upgrade by running
+              the command with --force or use the Kontena Pharos Pro version.
+            ERROR_MSG
+            signal_error error_message
           end
         end
         puts
