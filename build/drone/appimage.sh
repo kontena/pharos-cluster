7d85e47a3feeec1888d449f9ae5a847c3dd7765f
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-03-17 14:47:46 +0200

    build appimage

---
+++ build/drone/appimage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+apt-get update -y
+apt-get install -y build-essential libyaml-dev desktop-file-utils curl wget file
+curl -o /usr/local/bin/pkg2appimage https://raw.githubusercontent.com/AppImage/pkg2appimage/master/pkg2appimage
+chmod +x /usr/local/bin/pkg2appimage
+pkg2appimage ./build/travis/appimage.yml
