05181440c0c50c0b0a860e1b9870912f31f9d373
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-03-17 15:21:51 +0200

    try travis

--- build/travis/appimage.sh
+++ build/travis/appimage.sh
@@ -7,3 +7,4 @@ sudo apt-get install -y build-essential libyaml-dev desktop-file-utils curl wget
 sudo curl -o /usr/local/bin/pkg2appimage https://raw.githubusercontent.com/AppImage/pkg2appimage/master/pkg2appimage
 sudo chmod +x /usr/local/bin/pkg2appimage
 pkg2appimage ./build/travis/appimage.yml
+./out/pharos-cluster.glibc2.4-x86_64.AppImage version
