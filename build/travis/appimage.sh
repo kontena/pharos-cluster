898d7586e136077cf264a24b738881c5ea4214c9
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-03-17 15:13:00 +0200

    try travis

--- build/travis/appimage.sh
+++ build/travis/appimage.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-apt-get update -y
-apt-get install -y build-essential libyaml-dev desktop-file-utils curl wget file
+sudo apt-get update -y
+sudo apt-get install -y build-essential libyaml-dev desktop-file-utils curl wget file
 curl -o /usr/local/bin/pkg2appimage https://raw.githubusercontent.com/AppImage/pkg2appimage/master/pkg2appimage
 chmod +x /usr/local/bin/pkg2appimage
-pkg2appimage ./build/drone/appimage.yml
+pkg2appimage ./build/travis/appimage.yml
