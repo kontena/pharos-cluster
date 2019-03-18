06823cb39caf53911739e5e13ef71c90e2290c57
Jussi Nummelin <jnummelin@users.noreply.github.com>
2019-03-18 17:54:08 +0200

    Add missing flags for tf destroy (#1192)

--- lib/pharos/terraform/base_command.rb
+++ lib/pharos/terraform/base_command.rb
@@ -8,6 +8,9 @@ module Pharos
       options :yes?
 
       option "--workspace", "NAME", "terraform workspace", default: "default"
+      option "--var", "VAR", "set a variable in the terraform configuration.", multivalued: true
+      option "--var-file", "FILE", 'set variables in the terraform configuration from a file (default: terraform.tfvars or any .auto.tfvars)'
+      option "--state", "PATH", "Path to the state file. Defaults to 'terraform.tfstate'."
 
       def tf_workspace
         return 0 if run_cmd("terraform workspace select #{workspace} 2> /dev/null")
@@ -28,6 +31,18 @@ module Pharos
       # @return [Boolean]
       def run_cmd(cmd)
         system(cmd)
+      end
+
+      # Returns common options for both apply and destroy commands.
+      # @return [Array<String>]
+      def common_tf_options
+        opts = []
+        opts << "-auto-approve" if yes?
+        opts << "-state #{state}" if state
+        opts << "-var-file #{var_file}" if var_file
+        opts += var_list.map { |var| "-var #{var}" } if var_list
+
+        opts
       end
     end
   end
