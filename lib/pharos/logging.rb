7a952abd8acbefe8e39f94aa39bb3d0fe94be779
Kimmo Lehto <kimmo.lehto@kontena.io>
2019-03-19 14:27:07 +0200

    Increase exec error verbosity

--- lib/pharos/logging.rb
+++ lib/pharos/logging.rb
@@ -13,17 +13,11 @@ module Pharos
     end
 
     def self.format_exception(exc, severity = "ERROR")
-      return exc unless exc.is_a?(Exception)
-
-      if ENV["DEBUG"] || severity == "DEBUG"
-        message = exc.message.strip
+      if !ENV['DEBUG'].to_s.empty? || severity == "DEBUG"
         backtrace = "\n    #{exc.backtrace.join("\n    ")}"
-      else
-        message = exc.message[/\A(.+?)$/m, 1]
-        backtrace = nil
       end
 
-      "Error: #{message}#{backtrace}"
+      "Error: #{exc.message.strip}#{backtrace}"
     end
 
     def self.log_level
@@ -34,8 +28,9 @@ module Pharos
       @logger ||= Logger.new($stdout).tap do |logger|
         logger.progname = 'API'
         logger.level = Pharos::Logging.log_level
-        logger.formatter = proc do |_severity, _datetime, _progname, msg|
-          "    %<msg>s\n" % { msg: msg }
+        logger.formatter = proc do |severity, _datetime, _progname, msg|
+          message = msg.is_a?(Exception) ? Pharos::Logging.format_exception(msg, severity) : msg
+          "    %<msg>s\n" % { msg: message }
         end
       end
     end
