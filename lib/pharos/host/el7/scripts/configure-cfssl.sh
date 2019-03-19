a860dfb58b3a7f4bc8b7d6bfa521a04aee5dd862
Jussi Nummelin <jnummelin@users.noreply.github.com>
2019-03-19 13:34:08 +0200

    Update lib/pharos/host/el7/scripts/configure-cfssl.sh
    Co-Authored-By: jakolehm <jari.kolehmainen@gmail.com>

--- lib/pharos/host/el7/scripts/configure-cfssl.sh
+++ lib/pharos/host/el7/scripts/configure-cfssl.sh
@@ -11,7 +11,7 @@ if [ ! -f /usr/local/bin/cfssl ]; then
 fi
 
 if [ ! -f /usr/local/bin/cfssljson ]; then
-    curl -s -L -o /usr/local/bin/cfssljson "https://dl.bintray.com/kontena/pharos-bin/cfssl/cfssljson_linux-${ARCH}"
+    curl -s -L -o /usr/local/bin/cfssljson "https://dl.bintray.com/kontena/pharos-bin/cfssl/1.2/cfssljson_linux-${ARCH}"
     echo 1c9e628c3b86c3f2f8af56415d474c9ed4c8f9246630bd21c3418dbe5bf6401e /usr/local/bin/cfssljson | sha256sum -c
     chmod +x /usr/local/bin/cfssljson
 fi
