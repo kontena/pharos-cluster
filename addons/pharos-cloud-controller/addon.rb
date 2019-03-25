492f20ec57158df84ea6058316335264cef860d4
Jussi Nummelin <jussi@kontena.io>
2019-03-25 14:37:33 +0200

    fix formatting

--- addons/pharos-cloud-controller/addon.rb
+++ addons/pharos-cloud-controller/addon.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 Pharos.addon 'pharos-cloud-controller' do
-    version '0.1.0'
-    license 'Apache License 2.0'
+  version '0.1.0'
+  license 'Apache License 2.0'
 
-    install {
-        apply_resources
-      }
-
-  end
+  install {
+    apply_resources
+  }
+end
