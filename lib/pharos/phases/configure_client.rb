45e29686af479f598c1e68bf255a470ea988b503
Kimmo Lehto <kimmo.lehto@kontena.io>
2019-03-25 16:37:21 +0200

    Reduce changes

--- lib/pharos/phases/configure_client.rb
+++ lib/pharos/phases/configure_client.rb
@@ -8,22 +8,27 @@ module Pharos
       REMOTE_FILE = "/etc/kubernetes/admin.conf"
 
       def call
-        return unless kubeconfig_file.exist?
+        return unless kubeconfig?
 
         cluster_context['kubeconfig'] = kubeconfig
 
         client_prefetch
       end
 
-      # @return [Pharos::Transport::TransportFile]
-      def kubeconfig_file
-        @kubeconfig_file ||= transport.file(REMOTE_FILE)
+      # @return [String]
+      def kubeconfig?
+        transport.file(REMOTE_FILE).exist?
+      end
+
+      # @return [K8s::Config]
+      def read_kubeconfig
+        transport.file(REMOTE_FILE).read
       end
 
       # @return [K8s::Config]
       def kubeconfig
         logger.info { "Fetching kubectl config ..." }
-        config = YAML.safe_load(kubeconfig_file.read)
+        config = YAML.safe_load(read_kubeconfig)
 
         logger.debug { "New config: #{config}" }
         K8s::Config.new(config)
