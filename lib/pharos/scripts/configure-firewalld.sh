f5247c5d157ad3833ad6cc33901fef981e7de618
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-03-27 07:51:28 +0200

    fix output redir

--- lib/pharos/scripts/configure-firewalld.sh
+++ lib/pharos/scripts/configure-firewalld.sh
@@ -8,7 +8,7 @@ if ! systemctl is-active --quiet firewalld; then
 fi
 
 # reload only if this is first run
-if ! firewall-cmd --info-service pharos-worker 2&>1 /dev/null ; then
+if ! firewall-cmd --info-service pharos-worker > /dev/null 2>&1 ; then
     flock /var/run/xtables.lock -c "firewall-cmd --reload"
     sleep 10
 fi
@@ -16,13 +16,13 @@ fi
 if [ "$ROLE" = "master" ]; then
     firewall-cmd --permanent --add-service pharos-master
 fi
-if ! firewall-cmd --info-service pharos-worker 2&>1 /dev/null ; then
+if ! firewall-cmd --info-service pharos-worker > /dev/null 2>&1 ; then
     firewall-cmd --permanent --add-service pharos-worker
 fi
-if ! firewall-cmd --info-ipset pharos 2&>1 /dev/null ; then
+if ! firewall-cmd --info-ipset pharos > /dev/null 2>&1 ; then
     firewall-cmd --permanent --add-source ipset:pharos --zone trusted
 fi
-if ! firewall-cmd --query-masquerade 2&>1 /dev/null ; then
+if ! firewall-cmd --query-masquerade > /dev/null 2>&1 ; then
     firewall-cmd --add-masquerade --permanent
 fi
 
