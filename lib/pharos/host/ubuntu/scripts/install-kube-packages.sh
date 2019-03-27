db8023ac9faabde79bb36c6b561f26fdb29f9e0e
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-03-27 18:35:06 +0200

    cleanup

--- lib/pharos/host/ubuntu/scripts/install-kube-packages.sh
+++ lib/pharos/host/ubuntu/scripts/install-kube-packages.sh
@@ -3,9 +3,9 @@
 set -e
 
 export DEBIAN_FRONTEND=noninteractive
-apt-mark unhold kubectl kubeadm kubernetes-cni || echo "Nothing to unhold"
+apt-mark unhold kubectl kubeadm || echo "Nothing to unhold"
 apt-get install -y "kubectl=${KUBE_VERSION}-00" "kubeadm=${KUBEADM_VERSION}-00"
-apt-mark hold kubectl kubeadm kubernetes-cni
+apt-mark hold kubectl kubeadm
 
 # use KUBELET_EXTRA_ARGS from /etc/systemd/system/kubelet.service.d/11-pharos.conf instead
 sed -i 's/^KUBELET_EXTRA_ARGS=/#\0/' /etc/default/kubelet
