001600be38796b6a10e3d89e262b9d61553e62d4
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-03-27 12:02:57 +0200

    fix

--- lib/pharos/host/debian/scripts/install-kube-packages.sh
+++ lib/pharos/host/debian/scripts/install-kube-packages.sh
@@ -7,9 +7,6 @@ apt-mark unhold kubectl kubeadm || echo "Nothing to unhold"
 apt-get install -y "kubectl=${KUBE_VERSION}-00" "kubeadm=${KUBEADM_VERSION}-00"
 apt-mark hold kubectl kubeadm
 
-# use KUBELET_EXTRA_ARGS from /etc/systemd/system/kubelet.service.d/11-pharos.conf instead
-sed -i 's/^KUBELET_EXTRA_ARGS=/#\0/' /etc/default/kubelet
-
 if ! dpkg -s nfs-common > /dev/null; then
     systemctl mask rpcbind
     apt-get install -y nfs-common
