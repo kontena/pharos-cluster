2fe310218ab1f2ceca612f4fc7ee398fe0134880
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-03-27 11:36:22 +0200

    Fix firewalld reload issues (#1222)

--- lib/pharos/phases/configure_custom_network.rb
+++ lib/pharos/phases/configure_custom_network.rb
@@ -7,7 +7,14 @@ module Pharos
 
       def call
         logger.info { "Configuring custom network ..." }
-        stack = Pharos::Kube.stack('custom-network', @config.network.custom.manifest_path, name: 'custom_network', cluster_config: @config)
+        stack = Pharos::Kube.stack(
+          'custom-network',
+          @config.network.custom.manifest_path,
+          name: 'custom_network',
+          cluster_config: @config,
+          firewalld_enabled: !!@config.network&.firewalld&.enabled,
+          reload_iptables: !!cluster_context['reload-iptables']
+        )
         stack.apply(kube_client)
       end
     end
--- lib/pharos/scripts/configure-firewalld.sh
+++ lib/pharos/scripts/configure-firewalld.sh
@@ -7,11 +7,26 @@ if ! systemctl is-active --quiet firewalld; then
     systemctl start firewalld
 fi
 
-firewall-cmd --reload
+# reload only if this is first run
+if ! firewall-cmd --info-service pharos-worker > /dev/null 2>&1 ; then
+    flock /var/run/xtables.lock -c "firewall-cmd --reload"
+    sleep 10
+fi
+
 if [ "$ROLE" = "master" ]; then
     firewall-cmd --permanent --add-service pharos-master
 fi
-firewall-cmd --permanent --add-service pharos-worker
-firewall-cmd --permanent --add-source ipset:pharos --zone trusted
-firewall-cmd --add-masquerade --permanent
-firewall-cmd --reload
+if ! firewall-cmd --info-service pharos-worker > /dev/null 2>&1 ; then
+    firewall-cmd --permanent --add-service pharos-worker
+fi
+if ! firewall-cmd --info-ipset pharos > /dev/null 2>&1 ; then
+    firewall-cmd --permanent --add-source ipset:pharos --zone trusted
+fi
+if ! firewall-cmd --query-masquerade > /dev/null 2>&1 ; then
+    firewall-cmd --add-masquerade --permanent
+fi
+
+# reload only if this is first run
+if ! firewall-cmd --info-service pharos-worker > /dev/null 2>&1 ; then
+    flock /var/run/xtables.lock -c "firewall-cmd --reload"
+fi
