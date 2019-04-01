3f857e2a0161dbf4143a5f3237395c736df61b98
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-04-01 13:38:55 +0300

    fix

--- lib/pharos/host/debian/scripts/install-kube-packages.sh
+++ lib/pharos/host/debian/scripts/install-kube-packages.sh
@@ -3,9 +3,9 @@
 set -e
 
 export DEBIAN_FRONTEND=noninteractive
-apt-mark unhold kubectl kubeadm || echo "Nothing to unhold"
-apt-get install -y "kubectl=${KUBE_VERSION}-00" "kubeadm=${KUBEADM_VERSION}-00" "kubeadm=${KUBE_VERSION}-00" "kubernetes-cni=${CNI_VERSION}-00"
-apt-mark hold kubectl kubeadm
+apt-mark unhold kubectl kubeadm kubelet kubernetes-cni || echo "Nothing to unhold"
+apt-get install -y "kubectl=${KUBE_VERSION}-00" "kubeadm=${KUBEADM_VERSION}-00" "kubelet=${KUBE_VERSION}-00" "kubernetes-cni=${CNI_VERSION}-00"
+apt-mark hold kubectl kubeadm kubelet kubernetes-cni
 
 if ! dpkg -s nfs-common > /dev/null; then
     systemctl mask rpcbind
