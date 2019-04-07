3955d955b466a8861b58eaea17ba9791d2beecac
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-04-07 10:20:55 +0300

    travis

---
+++ e2e/footloose.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+curl -Lo footloose https://github.com/weaveworks/footloose/releases/download/0.3.0/footloose-0.3.0-linux-x86_64
+chmod +x footloose
+sudo mv footloose /usr/local/bin/
+
+pushd e2e/footloose
+sudo swapoff -a
+docker build -t footloose:bionic .
+footloose create
+popd
+
+bundle exec bin/pharos -c e2e/footloose/cluster.yml
