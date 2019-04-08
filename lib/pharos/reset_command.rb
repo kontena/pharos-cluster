761852e1c205260536ded468459536da4cc1194d
Kimmo Lehto <kimmo.lehto@gmail.com>
2019-04-08 08:40:37 +0300

    Fix reset command remaining masters validation (#1269)

--- lib/pharos/reset_command.rb
+++ lib/pharos/reset_command.rb
@@ -16,8 +16,10 @@ module Pharos
     end
 
     def reset_hosts
+      all_hosts = load_config.hosts.dup
       load_config.hosts.keep_if { |h| filtered_hosts.include?(h) }
-      if load_config.hosts.none?(&:master?)
+      remaining_masters = all_hosts.select(&:master?) - load_config.hosts.select(&:master?)
+      if remaining_masters.empty?
         signal_error 'There would be no master hosts left in the cluster after the reset. Reset the whole cluster by running this command without host filters.'
       elsif filtered_hosts.size > 1
         confirm_yes!("==> Do you really want to reset #{filtered_hosts.size} hosts #{filtered_hosts.map(&:address).join(',')} (data may be lost)?".bright_yellow, default: false)
@@ -26,10 +28,10 @@ module Pharos
       end
 
       start_time = Time.now
-      puts "==> Starting to reset hosts ...".green
+      puts "==> Starting to reset host#{'s' if filtered_hosts.size > 1} ...".green
       cluster_manager.apply_reset_hosts(filtered_hosts)
       reset_time = Time.now - start_time
-      puts "==> Hosts have been reset! (took #{humanize_duration(reset_time.to_i)})".green
+      puts "==> #{filtered_hosts.size > 1 ? 'Hosts have' : 'Host has'} been reset! (took #{humanize_duration(reset_time.to_i)})".green
     end
 
     def reset_all
