a3b7b413cab5061d3c4ffa556d579b184676033f
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-05-15 18:21:27 +0300

    improve container runtime reconfig with drain

--- non-oss/pharos_pro/phases/configure_host.rb
+++ non-oss/pharos_pro/phases/configure_host.rb
@@ -7,28 +7,38 @@ module Pharos
 
       def configure_container_runtime
         if @host.new? || host_configurer.configure_container_runtime_safe?
-          logger.info { "Configuring container runtime (#{@host.container_runtime}) packages ..." }
-          host_configurer.configure_container_runtime
+          logger.info "Configuring container runtime (#{@host.container_runtime}) packages ..."
+          host_configurer.configure_container_runtime!
         else
-          logger.info { "Reconfiguration of container runtime (#{@host.container_runtime}) might affect workloads, switching to a safe mode ..." }
-          mutex.synchronize do
-            if master_healthy?
-              logger.info { "Draining node ..." }
-              begin
-                drain_host
-              rescue Pharos::ExecError
-                drain_host!
-              end
-            end
-            logger.info { "Reconfiguring container runtime (#{@host.container_runtime}) packages ..." }
-            host_configurer.configure_container_runtime
-            if master_healthy?
-              logger.info { "Uncordoning node ..." }
-              sleep 1 until master_host.transport.exec("kubectl uncordon #{@host.hostname}")
-              logger.info { "Waiting for node to be ready ..." }
-              sleep 10 until master_host.transport.exec("kubectl get nodes -o jsonpath=\"{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}\" | grep 'Ready=True'").success?
-            end
+          concurrent_work('reconfigure_with_drain', (@config.hosts.size * 0.1).ceil ) do
+            logger.info "Reconfiguration of container runtime (#{@host.container_runtime}) might affect workloads, switching to a safe mode ..."
+            reconfigure_with_drain
           end
+        end
+      end
+
+      def reconfigure_with_drain
+        if master_healthy?
+          logger.info "Draining node ..."
+          begin
+            drain_host
+          rescue Pharos::ExecError
+            drain_host!
+          end
+        else
+          logger.warn "Cannot drain node because control-plane is not healthy ..."
+        end
+
+        logger.info "Waiting for volume umounts ..."
+        sleep 1 until @host.transport.exec("sudo mount | grep /var/lib/kubelet/plugins/ceph.rook.io/kontena-storage").error?
+
+        logger.info "Reconfiguring container runtime (#{@host.container_runtime}) packages ..."
+        host_configurer.configure_container_runtime!
+        if master_healthy?
+          logger.info "Uncordoning node ..."
+          sleep 1 until master_host.transport.exec("kubectl uncordon #{@host.hostname}").success?
+          logger.info "Waiting for node to be ready ..."
+          sleep 10 until master_host.transport.exec("kubectl get nodes -o jsonpath=\"{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}\" | grep 'Ready=True'").success?
         end
       end
 
