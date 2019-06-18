d5cffd11c261498eedd4ee6a14de92d0ee881631
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-06-18 08:09:04 +0300

    disable worker up e2e [cluster-e2e]

--- e2e/drone_setup.sh
+++ e2e/drone_setup.sh
@@ -10,7 +10,14 @@ fi
 cd e2e/digitalocean
 terraform init
 
-until terraform apply -auto-approve -var "cluster_name=${DRONE_BUILD_NUMBER}-do" -var "image=$1"
+WORKER_UP=${WORKER_UP:-false}
+if [ "${WORKER_UP}" = "true" ]; then
+  WORKER_UP_COUNT="1"
+else
+  WORKER_UP_COUNT="0"
+fi
+
+until terraform apply -auto-approve -var "cluster_name=${DRONE_BUILD_NUMBER}-do" -var "image=$1" -var "worker_up_count=${WORKER_UP_COUNT}"
 do
   echo "Apply failed... trying again in 5s"
   sleep 5
@@ -18,7 +25,7 @@ done
 
 terraform output -json > tf.json
 
-if [ "${WORKER_UP:-false}" = "true" ]; then
+if [ "${WORKER_UP}" = "true" ]; then
   jq ".worker_up.value.address[0]" tf.json | sed 's/"//g' > worker_up_address.txt
   jq ".pharos_hosts.value.masters[0].address[0]" tf.json | sed 's/"//g' > master_address.txt
 fi
