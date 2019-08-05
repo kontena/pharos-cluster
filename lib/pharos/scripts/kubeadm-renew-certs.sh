84edb188df086aa4e8aa612c77aa667f54e60959
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-08-05 17:57:30 +0300

    Fix apiserver cert recreate (#1437)

--- lib/pharos/scripts/kubeadm-renew-certs.sh
+++ lib/pharos/scripts/kubeadm-renew-certs.sh
@@ -6,4 +6,5 @@ if [ "$UNSET_PROXY" = "true" ]; then
   while read -r var; do unset "$var"; done < <(env | grep -i _proxy | sed 's/=.*//g')
 fi
 
+kubeadm init phase certs apiserver --config "${CONFIG}"
 kubeadm alpha certs renew apiserver --config "${CONFIG}"
