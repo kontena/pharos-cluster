fa029b3bb5fb4ace00207c1edb7a11b19b78133a
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-09-03 19:28:26 +0300

    Release v2.4.6 (#1476)

--- lib/pharos/host/debian/scripts/configure-firewalld.sh
+++ lib/pharos/host/debian/scripts/configure-firewalld.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+# shellcheck disable=SC1091
+. /usr/local/share/pharos/util.sh
+
+mkdir -p /etc/systemd/system/firewalld.service.d
+cat <<EOF >/etc/systemd/system/firewalld.service.d/10-pharos.conf
+[Service]
+Restart=always
+Before=kubelet.service
+EOF
+
 if ! dpkg -l firewalld > /dev/null; then
     export DEBIAN_FRONTEND=noninteractive
     apt-get install -y firewalld ipset ebtables
@@ -10,3 +20,5 @@ if ! dpkg -l firewalld > /dev/null; then
         systemctl start firewalld
     fi
 fi
+
+lineinfile "^CleanupOnExit=" "CleanupOnExit=no" "/etc/firewalld/firewalld.conf"
