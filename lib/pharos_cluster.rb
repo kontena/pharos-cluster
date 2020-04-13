d1fa158efd9e1469695faf0eb77d49b77f537271
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2020-04-13 09:37:56 +0300

    kubernetes v1.18.1
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- lib/pharos_cluster.rb
+++ lib/pharos_cluster.rb
@@ -8,11 +8,11 @@ require_relative "pharos/error"
 require_relative "pharos/root_command"
 
 module Pharos
-  KUBE_VERSION = ENV.fetch('KUBE_VERSION') { '1.18.0' }
+  KUBE_VERSION = ENV.fetch('KUBE_VERSION') { '1.18.1' }
   CNI_VERSION = '0.7.5'
   COREDNS_VERSION = '1.6.7'
-  DNS_NODE_CACHE_VERSION = '1.15.7'
-  ETCD_VERSION = ENV.fetch('ETCD_VERSION') { '3.3.10' }
+  DNS_NODE_CACHE_VERSION = '1.15.11'
+  ETCD_VERSION = ENV.fetch('ETCD_VERSION') { '3.4.3' }
   KUBEADM_VERSION = ENV.fetch('KUBEADM_VERSION') { KUBE_VERSION }
   KUBELET_PROXY_VERSION = '0.3.8'
 end
