27727f460f2a77465f1bc5dcc90b1e371d279461
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2020-04-30 07:44:51 +0300

    shellcheck
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- lib/pharos/host/el7/scripts/upgrade-kubeadm.sh
+++ lib/pharos/host/el7/scripts/upgrade-kubeadm.sh
@@ -13,7 +13,7 @@ tmpdir=$(mktemp -d)
 mkdir -p "$tmpdir"
 yum install "kubeadm-${VERSION}" -y --downloadonly --downloaddir="$tmpdir" --disableplugin=versionlock
 cd "$tmpdir"
-rpm2cpio *kubeadm*.rpm | cpio -idmv
+rpm2cpio ./*kubeadm*.rpm | cpio -idmv
 install -o root -g root -m 0755 -T ./usr/bin/kubeadm "/usr/local/bin/pharos-kubeadm-${VERSION}"
 rm -rf "$tmpdir"
 "/usr/local/bin/pharos-kubeadm-${VERSION}" version
