d59b39e47c3aef508f369cb7805bc5b333062c6f
techknowlogick <matti@mdranta.net>
2020-05-13 13:38:22 -0400

    Ubuntu 20.04 LTS Support (#1545)
    Signed-off-by: Matti R <matti@mdranta.net>

---
+++ lib/pharos/host/ubuntu/ubuntu_focal.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require_relative 'ubuntu'
+
+module Pharos
+  module Host
+    class UbuntuFocal < Ubuntu
+      register_config 'ubuntu', '20.04'
+
+      register_component(
+        name: 'docker', version: DOCKER_VERSION, license: 'Apache License 2.0',
+        enabled: proc { |c| c.hosts.any? { |h| h.container_runtime == 'docker' } }
+      )
+
+      register_component(
+        name: 'cfssl', version: CFSSL_VERSION, license: 'MIT',
+        enabled: proc { |c| !c.etcd&.endpoints }
+      )
+
+      # @return [Array<String>]
+      def kubelet_args
+        kubelet_args = super
+
+        kubelet_args
+      end
+
+      def configure_container_runtime
+        if docker?
+          exec_script(
+            'configure-docker.sh',
+            DOCKER_PACKAGE: 'docker-ce',
+            DOCKER_VERSION: DOCKER_VERSION,
+            INSECURE_REGISTRIES: insecure_registries
+          )
+        elsif custom_docker?
+          exec_script(
+            'configure-docker.sh',
+            INSECURE_REGISTRIES: insecure_registries
+          )
+        else
+          raise Pharos::Error, "Unknown container runtime: #{host.container_runtime}"
+        end
+      end
+
+      def default_repositories
+        [
+          Pharos::Configuration::Repository.new(
+            name: "pharos-kubernetes.list",
+            key_url: "https://packages.cloud.google.com/apt/doc/apt-key.gpg",
+            contents: "deb https://apt.kubernetes.io/ kubernetes-xenial main\n"
+          ),
+          Pharos::Configuration::Repository.new(
+            name: "docker-ce.list",
+            key_url: "https://download.docker.com/linux/ubuntu/gpg",
+            contents: "deb https://download.docker.com/linux/ubuntu bionic stable\n"
+          )
+        ]
+      end
+    end
+  end
+end
