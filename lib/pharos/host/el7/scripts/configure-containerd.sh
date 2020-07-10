bc187abb026aa404ce26dc2bbd8bc15a999be89c
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2020-07-10 11:58:23 +0300

    Containerd support for centos/rhel 7 (#1548)
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- lib/pharos/host/el7/scripts/configure-containerd.sh
+++ lib/pharos/host/el7/scripts/configure-containerd.sh
@@ -4,25 +4,24 @@ set -e
 
 # shellcheck disable=SC1091
 . /usr/local/share/pharos/util.sh
+# shellcheck disable=SC1091
+. /usr/local/share/pharos/el7.sh
 
 configure_container_runtime_proxy "containerd"
 
 if [ -z "$CONTAINERD_VERSION" ]; then
-    containerd -v
+    docker info
     exit 0
 fi
 
-export DEBIAN_FRONTEND=noninteractive
-
-apt-mark unhold containerd.io || echo "Nothing to unhold"
-if dpkg -l docker-ce ; then
-    apt-get install -y "containerd.io=$CONTAINERD_VERSION*" || echo "Cannot install specific version, keeping the current one"
-else
-    apt-get install -y "containerd.io=$CONTAINERD_VERSION*"
-fi
-apt-mark hold containerd.io
+yum_install_with_lock "containerd.io" "${CONTAINERD_VERSION}"
 
 lineinfile "^disabled_plugins =" "disabled_plugins = []" "/etc/containerd/config.toml"
+
+if ! systemctl is-active --quiet containerd; then
+    systemctl enable containerd
+    systemctl start containerd
+fi
 
 if ! systemctl is-active --quiet containerd; then
     systemctl enable containerd
