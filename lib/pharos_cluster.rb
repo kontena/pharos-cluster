c18e75dcdccfaf63f737998e4541538df07d776d
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2020-09-26 16:21:30 +0300

    cni 0.8.7
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- lib/pharos_cluster.rb
+++ lib/pharos_cluster.rb
@@ -9,7 +9,7 @@ require_relative "pharos/root_command"
 
 module Pharos
   KUBE_VERSION = ENV.fetch('KUBE_VERSION') { '1.18.8' }
-  CNI_VERSION = '0.8.6'
+  CNI_VERSION = '0.8.7'
   COREDNS_VERSION = '1.6.7'
   DNS_NODE_CACHE_VERSION = '1.15.11'
   ETCD_VERSION = ENV.fetch('ETCD_VERSION') { '3.4.3' }
